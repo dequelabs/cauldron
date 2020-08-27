4f9ffd6d4e84a388fbc5ceb9258f25962e371fb0
Harris Schneiderman <schne324@gmail.com>
2020-08-27 10:07:40 -0700

    class prefix

--- packages/react/src/components/Code/index.tsx
+++ packages/react/src/components/Code/index.tsx
@@ -4,6 +4,7 @@ import {
   Light as SyntaxHighlighter,
   SyntaxHighlighterProps
 } from 'react-syntax-highlighter';
+import classNames from 'classnames';
 import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
 import css from 'react-syntax-highlighter/dist/esm/languages/hljs/css';
 import xml from 'react-syntax-highlighter/dist/esm/languages/hljs/xml';
@@ -15,10 +16,19 @@ SyntaxHighlighter.registerLanguage('html', xml);
 interface Props extends SyntaxHighlighterProps {
   children: React.ReactNode;
   language?: 'javascript' | 'css' | 'html';
+  className?: string;
 }
 
-const Code: React.ComponentType<Props> = ({ children, ...props }) => (
-  <SyntaxHighlighter {...props} useInlineStyles={false}>
+const Code: React.ComponentType<Props> = ({
+  children,
+  className,
+  ...props
+}) => (
+  <SyntaxHighlighter
+    {...props}
+    useInlineStyles={false}
+    className={classNames('Code', className)}
+  >
     {children}
   </SyntaxHighlighter>
 );
@@ -27,7 +37,8 @@ Code.displayName = 'Code';
 
 Code.propTypes = {
   children: PropTypes.string.isRequired,
-  language: PropTypes.oneOf(['javascript', 'css', 'html'])
+  language: PropTypes.oneOf(['javascript', 'css', 'html']),
+  className: PropTypes.string
 };
 
 export default Code;
