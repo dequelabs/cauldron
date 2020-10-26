b9a9c92781581e5a185dcda3a93077d984119567
Harris Schneiderman <schne324@gmail.com>
2020-10-26 13:26:37 -0700

    linty lintersons

--- packages/react/src/utils/setRef.ts
+++ packages/react/src/utils/setRef.ts
@@ -5,6 +5,7 @@ export default function setRef<T>(ref: React.Ref<T>, element: T) {
   if (typeof ref === 'function') {
     ref(element);
   } else {
+    // eslint-disable-next-line
     // @ts-ignore
     // we need to overwrite the existing value in some special cases
     ref.current = element;
