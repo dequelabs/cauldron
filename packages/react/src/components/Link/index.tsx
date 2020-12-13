bb8880abaf3ea298fcb50047687ab0bed2091a97
Noah Cooper <noahcoop@umich.edu>
2020-12-13 18:42:16 -0500

    init commit react router links

--- packages/react/src/components/Link/index.tsx
+++ packages/react/src/components/Link/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+// @ts-ignore
+import { BrowserRouter, Link as RouterLink } from 'react-router-dom';
+
 export interface LinkProps
   extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   linkRef?: React.Ref<HTMLAnchorElement>;
+  reactRouter?: false;
   variant?: 'button';
+  useRouter?: false;
 }
 
 const Link = ({
@@ -13,24 +18,40 @@ const Link = ({
   linkRef,
   className,
   variant,
+  useRouter,
   ...other
-}: LinkProps) => (
-  <a
-    ref={linkRef}
-    className={classNames(className, {
-      Link: !variant,
-      'Button--primary': variant === 'button'
-    })}
-    {...other}
-  >
-    {children}
-  </a>
-);
+}: LinkProps) =>
+  useRouter ? (
+    <BrowserRouter>
+      <RouterLink
+        ref={linkRef}
+        className={classNames(className, {
+          Link: !variant,
+          'Button--primary': variant === 'button'
+        })}
+        {...other}
+      >
+        {children}
+      </RouterLink>
+    </BrowserRouter>
+  ) : (
+    <a
+      ref={linkRef}
+      className={classNames(className, {
+        Link: !variant,
+        'Button--primary': variant === 'button'
+      })}
+      {...other}
+    >
+      {children}
+    </a>
+  );
 
 Link.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
   variant: PropTypes.string,
+  useRouter: PropTypes.bool,
   linkRef: PropTypes.oneOfType([
     PropTypes.func,
     PropTypes.shape({ current: PropTypes.any })
