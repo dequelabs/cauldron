08f1209bded3fe5d9a19ebd8a8d6132a73652e8b
dequejosie <83524297+dequejosie@users.noreply.github.com>
2021-06-25 13:08:46 -0400

    update tests

--- packages/react/__tests__/src/components/TopBar/TopBar.js
+++ packages/react/__tests__/src/components/TopBar/TopBar.js
@@ -42,6 +42,20 @@ test('should return no axe violations', async () => {
   expect(await axe(topbar.html())).toHaveNoViolations();
 });
 
+test('should return no axe violations in light mode', async () => {
+  const topbar = mount(
+    <TopBar variant="light">
+      <div>LOGO</div>
+      <MenuBar>
+        <TopBarItem>1</TopBarItem>
+        <TopBarItem>2</TopBarItem>
+        <TopBarItem>3</TopBarItem>
+      </MenuBar>
+    </TopBar>
+  );
+  expect(await axe(topbar.html())).toHaveNoViolations();
+});
+
 test('renders TopBar className when no variant passed or variant is dark', () => {
   const topbar = mount(
     <TopBar>
