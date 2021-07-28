99a12b2ec07c2c3eabb6343e121207b0d181b90b
Harris Schneiderman <schne324@gmail.com>
2021-07-28 10:56:04 -0700

    fixes types to never use never

--- packages/react/src/components/Stepper/index.tsx
+++ packages/react/src/components/Stepper/index.tsx
@@ -10,54 +10,57 @@ interface BaseStepProps {
 
 interface StepWithChildren extends BaseStepProps {
   children: React.ReactNode;
-  tooltip?: never;
 }
 
 interface StepWithTooltip extends BaseStepProps {
   tooltip: React.ReactNode;
-  children?: never;
 }
 
 type StepProps = StepWithChildren | StepWithTooltip;
 
-export const Step = ({
-  children = null,
-  tooltip = null,
-  status,
-  className,
-  ...other
-}: StepProps) => (
-  <li
-    className={classNames(
-      'Stepper__step',
-      `Stepper__step--${status}`,
-      className
-    )}
-    aria-current={status === 'current' ? 'step' : 'false'}
-    {...other}
-  >
-    <div className="Stepper__step-line" />
-    <div className="Stepper__step-content">
-      {tooltip ? (
-        <TooltipTabstop
-          placement="bottom"
-          tooltip={tooltip}
-          // the pseudo content (ex: "1") is conveyed
-          // by the list item's position in the set of
-          // list items, therefore it is safe to clobber
-          // it with the contents of the tooltip in the
-          // tab stop's accessible name.
-          association="aria-labelledby"
-        >
-          <div className="Stepper__step-indicator" />
-        </TooltipTabstop>
-      ) : (
-        <div className="Stepper__step-indicator" />
+const isTooltip = (props: StepProps): props is StepWithTooltip => {
+  return !!(props as StepWithTooltip).tooltip;
+};
+
+export const Step = (props: StepProps) => {
+  const { status, className, ...other } = props;
+  return (
+    <li
+      className={classNames(
+        'Stepper__step',
+        `Stepper__step--${status}`,
+        className
       )}
-      {children && <div className="Stepper__step-label">{children}</div>}
-    </div>
-  </li>
-);
+      aria-current={status === 'current' ? 'step' : 'false'}
+      {...other}
+    >
+      <div className="Stepper__step-line" />
+      <div className="Stepper__step-content">
+        {isTooltip(props) ? (
+          <TooltipTabstop
+            placement="bottom"
+            tooltip={props.tooltip}
+            // the pseudo content (ex: "1") is conveyed
+            // by the list item's position in the set of
+            // list items, therefore it is safe to clobber
+            // it with the contents of the tooltip in the
+            // tab stop's accessible name.
+            association="aria-labelledby"
+          >
+            <div className="Stepper__step-indicator" />
+          </TooltipTabstop>
+        ) : (
+          <>
+            <div className="Stepper__step-indicator" />
+            {'children' in props && (
+              <div className="Stepper__step-label">{props.children}</div>
+            )}
+          </>
+        )}
+      </div>
+    </li>
+  );
+};
 
 Step.displayName = 'Step';
 
