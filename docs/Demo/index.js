f78736437aae8277d90b04b564884da1cd0289b8
Victor Gomex <75144504+vicdq@users.noreply.github.com>
2021-09-16 11:54:37 -0600

    feat: support for dark theme buttons and links (#321)

--- docs/Demo/index.js
+++ docs/Demo/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import jsxStringify from 'react-element-to-jsx-string';
 import { Code } from '@deque/cauldron-react';
@@ -10,69 +10,19 @@ const stringifyConfig = {
   showFunctions: true
 };
 
-class Demo extends Component {
-  render() {
-    const {
-      states,
-      component: Component,
-      propDocs,
-      children,
-      customImport
-    } = this.props;
-    const { displayName, defaultProps = {} } = Component;
+const Demo = props => {
+  const {
+    states,
+    component: Component,
+    propDocs,
+    children,
+    customImport
+  } = props;
 
-    return (
-      <div className="Demo">
-        <h1>{displayName}</h1>
-        <Code>
-          {customImport ||
-            `import { ${displayName} } from '@deque/cauldron-react'`}
-        </Code>
-        {states.length ? (
-          <div className="Demo-states">
-            <h2>Examples</h2>
-            {/* setting children to null in the key to avoid stringify choking on potential jsx children */}
-            {states.map(state => {
-              const {
-                DEMO_renderAfter,
-                DEMO_renderBefore,
-                DEMO_hide_renderAfterBefore = false,
-                ...thinState
-              } = state;
-              const componentMarkup = this.renderState(thinState);
-              const afterMarkup =
-                DEMO_renderAfter &&
-                !DEMO_hide_renderAfterBefore &&
-                jsxStringify(DEMO_renderAfter, stringifyConfig);
+  const { displayName, defaultProps = {} } = Component;
 
-              return (
-                <div key={componentMarkup}>
-                  {DEMO_renderBefore}
-                  <Component {...thinState} />
-                  {DEMO_renderAfter}
-                  <Code>
-                    {`${componentMarkup}${
-                      afterMarkup ? `\n${afterMarkup}` : ''
-                    }`}
-                  </Code>
-                </div>
-              );
-            })}
-            {children}
-          </div>
-        ) : (
-          children
-        )}
-        <div className="Demo-props">
-          <h2>Props</h2>
-          <PropDocs docs={propDocs} defaultProps={defaultProps} />
-        </div>
-      </div>
-    );
-  }
-
-  renderState = state => {
-    const { displayName } = this.props.component;
+  const renderState = state => {
+    const { displayName } = Component;
 
     if (!displayName) {
       throw new Error('Component missing displayName');
@@ -81,12 +31,59 @@ class Demo extends Component {
     const Tag = displayName;
     return jsxStringify(<Tag {...state} />, stringifyConfig);
   };
-}
+
+  return (
+    <div className="Demo">
+      <h1>{displayName}</h1>
+      <Code>
+        {customImport ||
+          `import { ${displayName} } from '@deque/cauldron-react'`}
+      </Code>
+      {states.length ? (
+        <div className="Demo-states">
+          <h2>Examples</h2>
+          {/* setting children to null in the key to avoid stringify choking on potential jsx children */}
+          {states.map(state => {
+            const {
+              DEMO_renderAfter,
+              DEMO_renderBefore,
+              DEMO_hide_renderAfterBefore = false,
+              ...thinState
+            } = state;
+            const componentMarkup = renderState(thinState);
+            const afterMarkup =
+              DEMO_renderAfter &&
+              !DEMO_hide_renderAfterBefore &&
+              jsxStringify(DEMO_renderAfter, stringifyConfig);
+
+            return (
+              <div key={componentMarkup}>
+                {DEMO_renderBefore}
+                <Component {...thinState} />
+                {DEMO_renderAfter}
+                <Code>
+                  {`${componentMarkup}${afterMarkup ? `\n${afterMarkup}` : ''}`}
+                </Code>
+              </div>
+            );
+          })}
+          {children}
+        </div>
+      ) : (
+        children
+      )}
+      <div className="Demo-props">
+        <h2>Props</h2>
+        <PropDocs docs={propDocs} defaultProps={defaultProps} />
+      </div>
+    </div>
+  );
+};
 
 Demo.propTypes = {
   propDocs: PropTypes.object.isRequired,
   states: PropTypes.arrayOf(PropTypes.object).isRequired,
-  component: PropTypes.func.isRequired,
+  component: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
   children: PropTypes.node,
   customImport: PropTypes.string
 };
