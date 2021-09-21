f179cb5b5bf85cee7c81b44a34f328d05193683d
Victor Gomez <victor.gomez@deque.com>
2021-09-21 10:24:22 -0600

    Transparent background to expand-collapse-panel and support for fullWidth property

--- packages/react/src/components/ExpandCollapsePanel/PanelTrigger.tsx
+++ packages/react/src/components/ExpandCollapsePanel/PanelTrigger.tsx
@@ -7,6 +7,7 @@ export interface PanelTriggerProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ((props: { open: boolean }) => React.ReactNode) | React.ReactNode;
   open?: boolean;
+  fullWidth?: string;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
@@ -14,13 +15,18 @@ function PanelTrigger({
   children,
   className,
   open,
+  fullWidth,
   onClick,
   ...other
 }: PanelTriggerProps) {
   return (
     <button
       {...other}
-      className={classnames('ExpandCollapse__trigger', className)}
+      className={classnames(
+        'ExpandCollapse__trigger',
+        fullWidth ? 'fullWidth' : '',
+        className
+      )}
       type="button"
       aria-expanded={open}
       onClick={onClick}
