ca69ce9cf76732961c23c08e4f8355b663c39885
dequejosie <83524297+dequejosie@users.noreply.github.com>
2021-09-23 18:28:19 -0400

    replace hidden

--- packages/react/src/components/Tabs/TabPanel.tsx
+++ packages/react/src/components/Tabs/TabPanel.tsx
@@ -4,24 +4,25 @@ import classNames from 'classnames';
 
 interface TabPanelProps extends React.HTMLAttributes<HTMLDivElement> {
   index: number;
-  value: number;
+  panelValue: number;
   children?: React.ReactNode;
   className?: string;
 }
 
 const TabPanel = (props: TabPanelProps) => {
-  const { children, index, value, className, ...other } = props;
+  const { children, index, panelValue, className, ...other } = props;
 
   return (
     <div
-      hidden={value !== index}
       id={`tabpanel-${index}`}
       aria-labelledby={`tab-${index}`}
       role="tabpanel"
-      className={classNames('TabPanel', className)}
+      className={classNames('TabPanel', className, {
+        'TabPanel--hidden': panelValue !== index
+      })}
       {...other}
     >
-      {value === index && children}
+      {children}
     </div>
   );
 };
@@ -29,7 +30,7 @@ const TabPanel = (props: TabPanelProps) => {
 TabPanel.displayName = 'TabPanel';
 TabPanel.Proptypes = {
   index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
+  panelValue: PropTypes.number.isRequired,
   children: PropTypes.node,
   className: PropTypes.string
 };
