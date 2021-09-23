2c5cd9d7d02ff188879ec14c4df5d391f4cbb09f
dequejosie <83524297+dequejosie@users.noreply.github.com>
2021-09-23 14:40:21 -0400

    fix more tests

--- packages/react/__tests__/src/components/Tabs/Tab.js
+++ packages/react/__tests__/src/components/Tabs/Tab.js
@@ -17,8 +17,8 @@ test('renders className Tab--active properly', () => {
   const TabWithFocus = mount(<Tab value={initialValue} index={0} />);
   const TabWithoutFocus = mount(<Tab value={initialValue} index={1} />);
 
-  expect(TabWithFocus.find('Tab--active')).toHaveLength(1);
-  expect(TabWithoutFocus.find('Tab--active')).toHaveLength(0);
+  expect(TabWithFocus.find('.Tab--active')).toHaveLength(1);
+  expect(TabWithoutFocus.find('.Tab--active')).toHaveLength(0);
 });
 
 test('renders tabIndex properly', async () => {
@@ -38,11 +38,11 @@ test('renders aria-selected properly', async () => {
 });
 
 test('focuses the tab whose value matches its index', async () => {
-  mount(<Tab value={initialValue} index={0} />);
+  mount(
+    <>
+      <Tab value={initialValue} index={0} />
+      <Tab value={initialValue} index={1} />
+    </>
+  );
   expect(document.activeElement.id).toEqual('tab-0');
-});
-
-test('does not focus the tab whose value does not match its index', async () => {
-  mount(<Tab value={initialValue} index={1} />);
-  expect(document.activeElement.outerHTML).toEqual('<body></body>');
 });
