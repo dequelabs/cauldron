de48681bd5b9af83002c5c8d7c91128045c8fbdf
dequejosie <83524297+dequejosie@users.noreply.github.com>
2021-09-27 16:03:02 -0400

    update Tab

--- packages/react/src/components/Tabs/Tab.tsx
+++ packages/react/src/components/Tabs/Tab.tsx
@@ -1,50 +1,21 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { useDidUpdate } from '../../index';
 
 interface TabProps extends React.HTMLAttributes<HTMLLIElement> {
-  value: number;
-  index: number;
   children?: React.ReactNode;
 }
 
-const Tab = ({ value, index, children }: TabProps) => {
-  const tabRef = useRef<HTMLLIElement>(null);
-  const tabIndex = index === value ? 0 : -1;
-  const selected = tabIndex === 0;
-
-  useDidUpdate(() => {
-    if (index !== value) {
-      return;
-    }
-
-    tabRef?.current?.focus();
-  }, [value]);
-
-  return (
-    <li
-      role="tab"
-      className={classNames('Tab', {
-        'Tab--active': selected
-      })}
-      id={`tab-${index}`}
-      aria-controls={`tabpanel-${index}`}
-      tabIndex={tabIndex}
-      aria-selected={selected}
-      ref={tabRef}
-    >
+const Tab = React.forwardRef<HTMLLIElement, TabProps>(
+  ({ children, ...other }: TabProps, ref) => (
+    <li ref={ref} role="tab" {...other}>
       {children}
     </li>
-  );
-};
+  )
+);
 
 Tab.displayName = 'Tab';
 Tab.propTypes = {
-  value: PropTypes.number.isRequired,
-  index: PropTypes.number.isRequired,
-  children: PropTypes.node,
-  label: PropTypes.string
+  children: PropTypes.node
 };
 
 export default Tab;
