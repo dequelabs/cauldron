45fd236c47580b891fdc32e4e868eef36f951b1e
dequejosie <83524297+dequejosie@users.noreply.github.com>
2021-10-06 18:18:33 -0400

    adjust css

--- docs/patterns/components/Tabs/index.js
+++ docs/patterns/components/Tabs/index.js
@@ -43,45 +43,51 @@ const Demo = () => {
       <h2>Code Sample</h2>
       <Code language="javascript">
         {`
-import React from 'react';
-import {
-  Tabs,
-  Tab,
-  TabPanel
-} from '@deque/cauldron-react';
+import React, { useRef } from 'react';
+import { Tabs, Tab, TabPanel, Code } from '@deque/cauldron-react';
+import './index.css';
 
-const Demo = () => (
-  <div>
-    <h1>Tabs</h1>
-    <h2>Demo</h2>
-    <h3>Basic Tabs</h3>
-    <Tabs aria-label="Basic Tabs">
-    <Tab>Tab 1</Tab>
-    <Tab>Tab 2</Tab>
-    <Tab>Tab 3</Tab>
-    <TabPanel>
-      <p>Insert content for tab panel 1 here...</p>
-    </TabPanel>
-    <TabPanel>
-      <p>Insert content for tab panel 2 here...</p>
-    </TabPanel>
-    <TabPanel>
-      <p>Insert content for tab panel 3 here...</p>
-    </TabPanel>
-  </Tabs>
-  <h3 id="tabs-variant">Full-width Tabs</h3>
-  <Tabs variant="full-width" aria-labelledby="tabs-variant">
-    <Tab>Tab 1</Tab>
-    <Tab>Tab 2</Tab>
-    <TabPanel>
-      <p>Insert content for tab panel 1 here...</p>
-    </TabPanel>
-    <TabPanel>
-      <p>Insert content for tab panel 2 here...</p>
-    </TabPanel>
-  </Tabs>
-  </div>
-);
+const Demo = () => {
+  const tabPanel1 = useRef(null);
+  const tabPanel2 = useRef(null);
+  const tabPanel3 = useRef(null);
+  const tabPanel4 = useRef(null);
+  const tabPanel5 = useRef(null);
+
+  return (
+    <div className="TabsDemo">
+      <h1>Tabs</h1>
+      <h2>Demo</h2>
+      <h3>Basic Tabs</h3>
+      <Tabs aria-label="Basic Tabs">
+        <Tab targetref={tabPanel1}>Tab 1</Tab>
+        <Tab targetref={tabPanel2}>Tab 2</Tab>
+        <Tab targetref={tabPanel3}>Tab 3</Tab>
+      </Tabs>
+      <TabPanel panelref={tabPanel1}>
+        <p>Insert content for tab panel 1 here...</p>
+      </TabPanel>
+      <TabPanel panelref={tabPanel2}>
+        <p>Insert content for tab panel 2 here...</p>
+      </TabPanel>
+      <TabPanel panelref={tabPanel3}>
+        <p>Insert content for tab panel 3 here...</p>
+      </TabPanel>
+
+      <h3 id="tabs-variant">Full-width Tabs</h3>
+      <Tabs variant="full-width" aria-labelledby="tabs-variant">
+        <Tab targetref={tabPanel5}>Tab 1</Tab>
+        <Tab targetref={tabPanel4}>Tab 2</Tab>
+      </Tabs>
+      <TabPanel panelref={tabPanel4}>
+        <p>Insert content for tab panel 2 here...</p>
+      </TabPanel>
+      <TabPanel panelref={tabPanel5}>
+        <p>Insert content for tab panel 1 here...</p>
+      </TabPanel>
+    </div>
+  );
+};
       `}
       </Code>
     </div>
