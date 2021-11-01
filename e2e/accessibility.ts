6de4c4fe3aa9c53149310fa5113a250d522d3eba
Stephen Mathieson <me@stephenmathieson.com>
2021-11-01 17:17:54 -0400

    test: Add E2E/a11y testing via `@axe-core/puppeteer`

---
+++ e2e/accessibility.ts
@@ -0,0 +1,90 @@
+import puppeteer from 'puppeteer';
+import { AxePuppeteer } from '@axe-core/puppeteer';
+import { createServer } from 'http-server';
+import { AxeResults } from 'axe-core';
+import logSymbols from 'log-symbols';
+import chalk from 'chalk';
+import fs from 'fs';
+import path from 'path';
+import assert from 'assert';
+
+const DIST_PATH = path.join(__dirname, '..', 'docs', 'dist');
+const INDEX_HTML = path.join(DIST_PATH, 'index.html');
+const PORT = 36981;
+const IS_CI = 'CI' in process.env;
+
+const main = async (): Promise<void> => {
+  assert(
+    fs.existsSync(DIST_PATH) && fs.existsSync(INDEX_HTML),
+    'Missing dist/'
+  );
+
+  const server = createServer({
+    cache: -1,
+    root: DIST_PATH
+  });
+
+  server.listen(PORT);
+
+  const results = new Map<string, AxeResults>();
+
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:${PORT}/`);
+
+  const links = await page.$$('.SideBar a[href]');
+  for (const link of links) {
+    const href = await link.getProperty('href');
+    const url = (await href?.jsonValue()) as string;
+
+    if (results.has(url)) {
+      console.warn('Already analyze page', { url });
+      return;
+    }
+
+    const p = await browser.newPage();
+
+    await p.goto(url);
+    const axe = new AxePuppeteer(p);
+    results.set(url, (await axe.analyze()) as AxeResults);
+    await p.close();
+  }
+
+  server.close();
+  await page.close();
+  await browser.close();
+
+  // In CI, don't try to measure; use 2x the longest template name instead.
+  const maxWidth = IS_CI
+    ? 2 * Math.max(0, ...[...results.keys()].map(t => t.length))
+    : process.stdout.columns - 8;
+
+  let foundViolations = false;
+
+  for (const [url, { violations }] of results) {
+    let symbol = logSymbols.success;
+    if (violations.length) {
+      symbol = logSymbols.warning;
+      foundViolations = true;
+    }
+
+    const file = chalk.cyan(chalk.bold(url));
+    const dots = '.'.repeat(maxWidth - url.length - symbol.length);
+    console.log(file, dots, symbol);
+
+    for (const { id, help } of violations) {
+      console.log('↳', chalk.underline(chalk.magenta(id)), '━', help);
+    }
+  }
+
+  if (foundViolations) {
+    console.log();
+    throw new Error('Found accessibility violations');
+  }
+};
+
+main().catch((error: Error) => {
+  console.error(error);
+  process.exit(1);
+});
