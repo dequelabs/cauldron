2f7b3550ae8d4897c0fedced3864152b9a080c54
dequejosie <83524297+dequejosie@users.noreply.github.com>
2021-11-03 15:37:31 -0400

    add folder again

---
+++ packages/react/src/components/NavBar/NavBarTrigger.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Icon from '../Icon';
+
+interface NavBarTriggerProps extends React.HTMLAttributes<HTMLLIElement> {
+  children: React.ReactNode;
+  show: boolean;
+  handleTriggerClick: () => void;
+}
+
+const NavBarTrigger: React.ComponentType<NavBarTriggerProps> = ({
+  children,
+  className,
+  show,
+  handleTriggerClick = () => {},
+  ...other
+}) => (
+  <li
+    aria-haspopup="true"
+    className={classNames('NavBar__menu-trigger', className, {
+      'NavBar__menu-trigger--active': show
+    })}
+    {...other}
+  >
+    <button
+      aria-haspopup="true"
+      aria-expanded={show}
+      onClick={handleTriggerClick}
+    >
+      <Icon type={show ? 'close' : 'hamburger-menu'} />
+      {children}
+    </button>
+  </li>
+);
+
+NavBarTrigger.displayName = 'NavBarTrigger';
+NavBarTrigger.propTypes = {
+  children: PropTypes.node.isRequired,
+  show: PropTypes.bool.isRequired,
+  handleTriggerClick: PropTypes.func.isRequired,
+  className: PropTypes.string
+};
+
+export default NavBarTrigger;
---
+++ packages/react/src/components/NavBar/index.ts
@@ -0,0 +1,3 @@
+export { default } from './NavBar';
+export { default as NavItem } from './NavItem';
+export { default as NavBarTrigger } from './NavBarTrigger';
