e6489fce8a7a54a285ca9ca1c077eb4f44ffc8ae
dequejosie <83524297+dequejosie@users.noreply.github.com>
2021-11-05 10:22:33 -0400

    more edits

--- docs/patterns/components/NavBar/index.js
+++ docs/patterns/components/NavBar/index.js
@@ -3,7 +3,7 @@ import { NavBar, NavItem, Code } from '@deque/cauldron-react';
 import './index.css';
 
 const Demo = () => {
-  const componentsList = new Array(6).fill('NavItem');
+  const componentsList = new Array(5).fill('NavItem');
 
   return (
     <div className="NavBarDemo">
@@ -35,25 +35,38 @@ const Demo = () => {
       <Code language="javascript">
         {`
 import React from 'react';
-import { NavBar, NavItem } from '@deque/cauldron-react';
+import { NavBar, NavItem, Code } from '@deque/cauldron-react';
+import './index.css';
 
 const Demo = () => {
-  const componentsList = new Array(6).fill('NavItem');
+  const componentsList = new Array(5).fill('NavItem');
 
   return (
     <div className="NavBarDemo">
       <h1>NavBar</h1>
       <h2>Demo</h2>
-      <h3>NavBar</h3>
+      <h3>Basic NavBar</h3>
       <NavBar>
         {componentsList.map(name => {
           return (
             <NavItem key={name}>
-              <a>{name}</a>
+              <a href="#">{name}</a>
             </NavItem>
           );
         })}
       </NavBar>
+      <h3>Collapsed NavBar</h3>
+      <div className="container">
+        <NavBar collapsed>
+          {componentsList.map(name => {
+            return (
+              <NavItem key={name}>
+                <a href="#">{name}</a>
+              </NavItem>
+            );
+          })}
+        </NavBar>
+      </div>
     </div>
        `}
       </Code>
