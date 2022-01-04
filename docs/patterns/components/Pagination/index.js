44998952a65bb37724039d2ee80982367a806a73
Harris Schneiderman <schne324@gmail.com>
2022-01-04 10:46:31 -0800

    PROP DOC AND LOCK IT

--- docs/patterns/components/Pagination/index.js
+++ docs/patterns/components/Pagination/index.js
@@ -45,8 +45,89 @@ const PaginationDemo = () => {
           totalItems: {
             type: 'number',
             description:
-              'The total number of items being rendered (not the total number of pages)',
+              'The total number of items being rendered (not the total number of pages).',
             required: true
+          },
+          itemsPerPage: {
+            type: 'number',
+            description: 'The total number of items per page.',
+            required: false,
+            default: 10
+          },
+          currentPage: {
+            type: 'number',
+            description: 'The (1-based) number of the current page.',
+            required: false,
+            default: 1
+          },
+          statusLabel: {
+            type: 'ReactNode',
+            description:
+              'The "x of y" status label (rendered as text in the middle of the pagination controls).',
+            required: false,
+            default: `<span>
+                Showing <strong>{itemStart}</strong> of{' '}
+                <strong>{itemEnd}</strong> of <strong>{totalItems}</strong>
+              </span>`
+          },
+          firstPageLabel: {
+            type: 'string',
+            description:
+              'The label text for the first page button (to be rendered as a tooltip).',
+            required: false,
+            default: 'First page'
+          },
+          previousPageLabel: {
+            type: 'string',
+            description:
+              'The label text for the previous page button (to be rendered as a tooltip).',
+            required: false,
+            default: 'Previous page'
+          },
+          nextPageLabel: {
+            type: 'string',
+            description:
+              'The label text for the next page button (to be rendered as a tooltip).',
+            required: false,
+            default: 'Next page'
+          },
+          lastPageLabel: {
+            type: 'string',
+            description:
+              'The label text for the last page button (to be rendered as a tooltip).',
+            required: false,
+            default: 'Last page'
+          },
+          onNextPageClick: {
+            type: 'function',
+            description:
+              'Function to be called when next page button is clicked',
+            required: false
+          },
+          onPreviousPageClick: {
+            type: 'function',
+            description:
+              'Function to be called when previous page button is clicked',
+            required: false
+          },
+          onFirstPageClick: {
+            type: 'function',
+            description:
+              'Function to be called when first page button is clicked',
+            required: false
+          },
+          onLastPageClick: {
+            type: 'function',
+            description:
+              'Function to be called when last page button is clicked',
+            required: false
+          },
+          tooltipPlacement: {
+            type: 'string',
+            description:
+              'The position of the tooltip relative to its target element.',
+            required: false,
+            defaultValue: "'bottom'"
           }
         }}
       />
