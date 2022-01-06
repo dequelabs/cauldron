f7d9078ab8013091fc862a34b0880e9026d71fd2
Jason <jason@scurker.com>
2022-01-06 09:59:17 -0600

    feat(react): implement new loader and variants (#471)

--- docs/patterns/components/Loader/index.js
+++ docs/patterns/components/Loader/index.js
@@ -6,12 +6,16 @@ import { className } from '../../../props';
 const LoaderDemo = () => (
   <Demo
     component={Loader}
-    states={[{ label: 'Loading...' }, {}]}
+    states={[{ label: 'Loading...' }, { variant: 'large' }]}
     propDocs={{
       label: {
         type: 'string',
         description:
           'The desired label will be rendered offscreen. If not provided, aria-hidden="true" will be applied to the element.'
+      },
+      variant: {
+        type: 'string',
+        description: 'Loader variant, can be "small" or "large".'
       },
       className
     }}
