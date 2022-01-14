0e623a4b05c54ca1052ca73a531ca2694f304b4c
dequejosie <83524297+dequejosie@users.noreply.github.com>
2022-01-14 15:49:13 -0500

    feat: Make Table support sorting

--- docs/patterns/components/Table/index.js
+++ docs/patterns/components/Table/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Demo from '../../../Demo';
 import {
   Table,
@@ -19,63 +19,110 @@ const tableComponentNames = [
   'TableRow'
 ];
 
-const TableDemo = () => (
-  <Demo
-    component={Table}
-    customImport={`import {\n  ${tableComponentNames.join(
-      ',\n  '
-    )}\n} from '@deque/cauldron-react'`}
-    states={[
-      {
-        children: (
-          <>
-            <TableHead>
-              <TableRow>
-                <TableHeader scope="col">First Name</TableHeader>
-                <TableHeader scope="col">Last Name</TableHeader>
-                <TableHeader scope="col">Email</TableHeader>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>Frank</TableCell>
-                <TableCell>Zappa</TableCell>
-                <TableCell>frank@zappa.io</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Duane</TableCell>
-                <TableCell>Allman</TableCell>
-                <TableCell>duane@almond.biz</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Yamandu</TableCell>
-                <TableCell>Costa</TableCell>
-                <TableCell>yamandu_costa@gmail.br</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Jimmy</TableCell>
-                <TableCell>Herring</TableCell>
-                <TableCell>jamesHerring@hotmail.gov</TableCell>
-              </TableRow>
-            </TableBody>
-          </>
-        )
-      }
-    ]}
-    propDocs={{
-      children: {
-        ...children,
-        required: true
-      },
-      className
-    }}
-  >
-    <p>
-      <strong>NOTE:</strong> the table width by default is set to 100%. If a
-      max-width is desired, you can simply use your own CSS to apply it.
-    </p>
-  </Demo>
-);
+const sampleData = [
+  {
+    first_name: 'Frank',
+    last_name: 'Zappa',
+    email: 'frank@zappa.io'
+  },
+  {
+    first_name: 'Duane',
+    last_name: 'Allman',
+    email: 'duane@almond.biz'
+  },
+  {
+    first_name: 'Yamandu',
+    last_name: 'Costa',
+    email: 'yamandu_costa@gmail.br'
+  },
+  {
+    first_name: 'Jimmy',
+    last_name: 'Herring',
+    email: 'jamesHerring@hotmail.gov'
+  }
+];
+
+const TableDemo = () => {
+  const [sort, setSort] = useState([null, 'none']);
+  const [sortBy, sortDir] = sort;
+
+  const comparator = data =>
+    data.sort((a, b) =>
+      (sortDir === 'ascending' ? a[sortBy] : b[sortBy]).localeCompare(
+        sortDir === 'ascending' ? b[sortBy] : a[sortBy]
+      )
+    );
+
+  const sortedData = sortBy ? comparator(sampleData) : sampleData;
+
+  const getSortDir = column => (column === sortBy ? sortDir : 'none');
+  /* asc => desc
+     none => asc
+     desc => asc */
+  const setSortDir = column =>
+    column === sortBy && sortDir === 'ascending' ? 'descending' : 'ascending';
+
+  return (
+    <Demo
+      component={Table}
+      customImport={`import {\n  ${tableComponentNames.join(
+        ',\n  '
+      )}\n} from '@deque/cauldron-react'`}
+      states={[
+        {
+          children: (
+            <>
+              <TableHead>
+                <TableRow>
+                  <TableHeader scope="col">First Name</TableHeader>
+                  <TableHeader
+                    scope="col"
+                    sortDir={getSortDir('last_name')}
+                    onSort={() => {
+                      setSort(['last_name', setSortDir('last_name')]);
+                    }}
+                  >
+                    Last Name
+                  </TableHeader>
+                  <TableHeader
+                    scope="col"
+                    sortDir={getSortDir('email')}
+                    onSort={() => {
+                      setSort(['email', setSortDir('email')]);
+                    }}
+                  >
+                    Email
+                  </TableHeader>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {sortedData.map(contact => (
+                  <TableRow>
+                    <TableCell>{contact.first_name}</TableCell>
+                    <TableCell>{contact.last_name}</TableCell>
+                    <TableCell>{contact.email}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </>
+          )
+        }
+      ]}
+      propDocs={{
+        children: {
+          ...children,
+          required: true
+        },
+        className
+      }}
+    >
+      <p>
+        <strong>NOTE:</strong> the table width by default is set to 100%. If a
+        max-width is desired, you can simply use your own CSS to apply it.
+      </p>
+    </Demo>
+  );
+};
 
 TableDemo.displayName = 'TableDemo';
 export default TableDemo;
--- packages/react/src/components/Table/TableHeader.tsx
+++ packages/react/src/components/Table/TableHeader.tsx
@@ -1,21 +1,49 @@
 import React, { ThHTMLAttributes } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Icon from '../Icon';
+
+// these match aria-sort's values
+type SortDirection = 'ascending' | 'descending' | 'none';
 
 interface TableHeaderProps extends ThHTMLAttributes<HTMLTableCellElement> {
   children: React.ReactNode;
+  sortDir?: SortDirection;
+  onSort?: () => void;
   className?: string;
 }
 
-const TableHeader = ({ children, className, ...other }: TableHeaderProps) => (
-  <th className={classNames('TableHeader', className)} {...other}>
-    {children}
+const TableHeader = ({
+  children,
+  sortDir,
+  onSort,
+  className,
+  ...other
+}: TableHeaderProps) => (
+  <th
+    aria-sort={sortDir}
+    className={classNames('TableHeader', className)}
+    {...other}
+  >
+    {onSort && sortDir ? (
+      <button onClick={onSort}>
+        {children}
+        {['none', 'ascending'].includes(sortDir) && <Icon type="triangle-up" />}
+        {['none', 'descending'].includes(sortDir) && (
+          <Icon type="triangle-down" />
+        )}
+      </button>
+    ) : (
+      children
+    )}
   </th>
 );
 
 TableHeader.displayName = 'TableHeader';
 TableHeader.propTypes = {
   children: PropTypes.node.isRequired,
+  sortDir: PropTypes.string,
+  onSort: PropTypes.func,
   className: PropTypes.string
 };
 
