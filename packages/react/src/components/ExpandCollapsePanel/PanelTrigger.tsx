b10ba7836ed9f9377daf18d12e6de16027119337
Tom Busillo <tom.busillo@deque.com>
2022-06-09 16:36:08 -0400

    removes unnecessary import

--- packages/react/src/components/ExpandCollapsePanel/PanelTrigger.tsx
+++ packages/react/src/components/ExpandCollapsePanel/PanelTrigger.tsx
@@ -1,4 +1,4 @@
-import React, { ElementType, forwardRef, Ref } from 'react';
+import React, { forwardRef, Ref } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Icon, { IconType } from '../Icon';
