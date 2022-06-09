f7a2cc0bbac26e035807a90b5f7b3a3ec64a97fe
Tom Busillo <tom.busillo@deque.com>
2022-06-09 12:42:16 -0400

    fixes styling and adds to docs

--- docs/patterns/components/Accordion/index.js
+++ docs/patterns/components/Accordion/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Demo from '../../../Demo';
 import {
   Accordion,
   AccordionContainer,
@@ -8,90 +7,62 @@ import {
   Code
 } from '@deque/cauldron-react/';
 import classNames from 'classnames';
+import PropDocs from '../../../Demo/PropDocs';
 import './index.css';
 import { children, className } from '../../../props';
-
-const accordionComponentNames = [
-  'Accordion',
-  'AccordionContainer',
-  'AccordionTrigger',
-  'AccordionContent'
-];
 
 export const AccordionDemo = () => {
   return (
     <div>
       <div className="data-list-demo">
-        <Demo
-          component={Accordion}
-          customImport={`import {\n  ${accordionComponentNames.join(
-            ',\n  '
-          )}\n} from '@deque/cauldron-react'`}
-          propDocs={{
-            ...children,
-            children: {
-              description:
-                'Only required for <Address>. If children is undefined for <AddressLine>, it will not render.'
-            },
-            className,
-            city: {
-              type: 'node',
-              description:
-                '<AddressCityStateZip> Only. The city to be combined with state and zip in a single line.'
-            },
-            state: {
-              type: 'node',
-              description:
-                '<AddressCityStateZip> Only. The state to be combined with city and zip in a single line.'
-            },
-            zip: {
-              type: 'node',
-              description:
-                '<AddressCityStateZip> Only. The zip code to be combined with city and state in a single line.'
-            }
-          }}
-          states={[]}
-        >
-          <h2>Demo</h2>
-          <h3>Controlled</h3>
+        <h1>Accordion</h1>
+        <h2>Demo</h2>
+        <h3>Controlled</h3>
 
-          <p>
-            If you need to manually control the open/closed state of the panel,
-            you can do this by setting the open prop and handling changes with
-            onToggle.
-          </p>
-          <ControlledAccordion label="Accordion #1" />
-          <ControlledAccordion label="Accordion #2" />
-          <ControlledAccordion label="Accordion #3" />
+        <p>
+          If you need to manually control the open/closed state of the panel,
+          you can do this by setting the open prop and handling changes with
+          onToggle.
+        </p>
+        <ControlledAccordion label="Accordion #1" />
+        <ControlledAccordion label="Accordion #2" />
+        <ControlledAccordion label="Accordion #3" />
 
-          <h3>Uncontrolled</h3>
+        <h3>Uncontrolled</h3>
 
-          <Accordion>
-            <AccordionContainer>
-              <AccordionTrigger>Accordion #1</AccordionTrigger>
-              <AccordionContent>Here is some content</AccordionContent>
-            </AccordionContainer>
-          </Accordion>
+        <Accordion>
+          <AccordionContainer>
+            <AccordionTrigger>Accordion #1</AccordionTrigger>
+            <AccordionContent>Here is some content</AccordionContent>
+          </AccordionContainer>
+        </Accordion>
 
-          <Accordion>
-            <AccordionContainer>
-              <AccordionTrigger>Accordion #2</AccordionTrigger>
-              <AccordionContent>Here is some content</AccordionContent>
-            </AccordionContainer>
-          </Accordion>
+        <Accordion>
+          <AccordionContainer>
+            <AccordionTrigger>Accordion #2</AccordionTrigger>
+            <AccordionContent>Here is some content</AccordionContent>
+          </AccordionContainer>
+        </Accordion>
 
-          <Accordion>
-            <AccordionContainer>
-              <AccordionTrigger>Accordion #3</AccordionTrigger>
-              <AccordionContent>Here is some content</AccordionContent>
-            </AccordionContainer>
-          </Accordion>
+        <Accordion>
+          <AccordionContainer>
+            <AccordionTrigger>Accordion #3</AccordionTrigger>
+            <AccordionContent>Here is some content</AccordionContent>
+          </AccordionContainer>
+        </Accordion>
 
-          <h2>Code Sample</h2>
-          <h3>Controlled</h3>
-          <Code role="region" tabIndex={0}>
-            {`const ControlledAccordion = ({ label }) => {
-  
+        <h2>Code Sample</h2>
+        <h3>Controlled</h3>
+        <Code role="region" tabIndex={0}>
+          {`import React, { useEffect, useState } from 'react';
+  import {
+  Accordion,
+  AccordionContainer,
+  AccordionTrigger,
+  AccordionContent,
+} from '@deque/cauldron-react/';
+
+const ControlledAccordion = ({ label }) => {
   const [open, setIsOpen] = useState(false);
 
   return (
@@ -119,23 +90,87 @@ export const AccordionDemo = () => {
     </Accordion>
   );
 };`}
-          </Code>
-          <p>
-            Then the controlled component would be called with something like:
-          </p>
-          <Code role="region" tabIndex={0}>
-            {`<ControlledAccordion label="Accordion #1" />`}
-          </Code>
-          <h3>Uncontrolled</h3>
-          <Code role="region" tabIndex={0}>
-            {`<Accordion>
+        </Code>
+        <p>
+          Then the controlled component would be called with something like:
+        </p>
+        <Code role="region" tabIndex={0}>
+          {`<ControlledAccordion label="Accordion #1" />`}
+        </Code>
+        <h3>Uncontrolled</h3>
+        <Code role="region" tabIndex={0}>
+          {`<Accordion>
   <AccordionContainer>
     <AccordionTrigger>Accordion #3</AccordionTrigger>
     <AccordionContent>Here is some content</AccordionContent>
   </AccordionContainer>
 </Accordion>`}
-          </Code>
-        </Demo>
+        </Code>
+      </div>
+      <div className="Demo-props">
+        <h2>Props</h2>
+        <h3>
+          <code>ExpandCollapsePanel</code>
+        </h3>
+
+        <PropDocs
+          docs={{
+            className,
+            children,
+            open: {
+              type: 'boolean',
+              description: 'Initial collapsed state of ExpandCollapsePanel',
+              default: 'false'
+            },
+            animationTiming: {
+              type: 'number | boolean',
+              description:
+                'Animation time of expand/collapse in ms. Animation disabled when set to false.',
+              default: 250
+            },
+            onToggle: {
+              type: '(e: React.MouseEvent<HTMLButtonElement>) => void',
+              description:
+                'onToggle handler for the panel. The original event object will be passed.',
+              default: 'function () {}'
+            }
+          }}
+        />
+
+        <h3>
+          <code>PanelTrigger</code>
+        </h3>
+        <PropDocs
+          docs={{
+            children,
+            className,
+            open: {
+              type: 'boolean',
+              description: 'Initial collapsed state of PanelTrigger',
+              default: 'false'
+            },
+            fullWidth: {
+              type: 'string',
+              description:
+                'When set to "fullWidth" this component will stretch to the full width of its parent.'
+            },
+            onClick: {
+              type: '(e: React.MouseEvent<HTMLButtonElement>)',
+              description:
+                'onClick handler for PanelTrigger. The original event object will be passed.'
+            },
+            iconExpanded: {
+              type: 'string',
+              description: 'The Icon to use when open=true.',
+              default: 'chevron-down'
+            },
+            iconCollapsed: {
+              type: 'string',
+              description: 'The Icon to use when open=false.',
+              default: 'chevron-right'
+            }
+          }}
+        />
       </div>
     </div>
   );
@@ -155,10 +190,7 @@ const ControlledAccordion = ({ label }) => {
         isControlled
       >
         <AccordionTrigger
-          className={classNames(
-            'Accordion__trigger underline',
-            open ? 'expanded' : ''
-          )}
+          className={classNames('Accordion__trigger underline')}
           iconExpanded="triangle-down"
           iconCollapsed="triangle-right"
         >
