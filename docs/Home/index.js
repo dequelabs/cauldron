a753a94878db895024c6baba2c11c735c844aba9
Abbey Perini <abbey.perini@deque.com>
2022-08-19 12:37:45 -0400

    docs: homepage content
    - The purpose of the resource
    Guidelines for how to use it
    - Who made it (Deque)
    - A path to submit contributions and bugs
    - A call to action to try Deque tools to test for a11y
    
    Issue #738

--- docs/Home/index.js
+++ docs/Home/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Code } from '@deque/cauldron-react';
+import { Code, Link } from '@deque/cauldron-react';
 
 const Home = () => (
   <div>
-    <h1>Cauldron React</h1>
+    <h1>Cauldron React by Deque Systems</h1>
+    <h2>Purpose</h2>
+    <p>
+      These packages include everything from typography and colors to components
+      like custom form controls. The design and interactions are intended to
+      show how Deque would solve these problems. It's how we approach them in
+      our prouducts.
+    </p>
+    <h2>Usage</h2>
+    <p>
+      Cauldron React is an accessible React components library. Cauldron Styles
+      contains accessible styling for those components and more. Select
+      component names in the sidebar for demos!
+    </p>
     <h2>Installation</h2>
     <Code>
       {'$ npm install --save @deque/cauldron-react @deque/cauldron-styles'}
     </Code>
-    <h2>Usage</h2>
     <Code language="javascript" role="region" tabIndex={0}>
       {`
 import { Workspace, Button } from '@deque/cauldron-react';
@@ -23,6 +35,25 @@ const Foo = () => (
 );
       `}
     </Code>
+    <h2>Contribute</h2>
+    <p>
+      Check out our{' '}
+      <Link href="https://github.com/dequelabs/cauldron/blob/develop/CONTRIBUTING.md">
+        contribution guide!
+      </Link>
+    </p>
+    <p>
+      Found a bug?{' '}
+      <Link href="https://github.com/dequelabs/cauldron/issues">
+        Open an issue!
+      </Link>
+    </p>
+    <h2>More Accessibility Tools</h2>
+    <p>
+      Don't stop here! Check out{' '}
+      <Link href="https://www.deque.com/axe/">Deque Systems' axe Tools</Link>{' '}
+      for all your accessibility testing needs!
+    </p>
   </div>
 );
 
