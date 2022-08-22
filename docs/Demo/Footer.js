d78447a5c7e62e13850b97f3e63adee79e380287
Abbey Perini <abbey.perini@deque.com>
2022-08-22 12:53:38 -0400

    docs: typo

--- docs/Demo/Footer.js
+++ docs/Demo/Footer.js
@@ -13,7 +13,7 @@ const Footer = props => {
           <Link href="https://www.deque.com/terms-of-use/">Terms of Use</Link>
         </li>
         <li>
-          <Link href="https://www.mozilla.org/en-US/MPL/2.0/ ">
+          <Link href="https://www.mozilla.org/en-US/MPL/2.0/">
             MPL-2.0 License
           </Link>
         </li>
