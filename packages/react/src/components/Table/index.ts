aee7d22774033b215fb2d77d4a9343ba698c9e1d
Michael <michaelsiek094@gmail.com>
2023-01-23 10:24:51 -0500

    feat(table): create table footer component (#864)

--- packages/react/src/components/Table/index.ts
+++ packages/react/src/components/Table/index.ts
@@ -4,3 +4,4 @@ export { default as TableCell } from './TableCell';
 export { default as TableHead } from './TableHead';
 export { default as TableHeader } from './TableHeader';
 export { default as TableRow } from './TableRow';
+export { default as TableFooter } from './TableFooter';
