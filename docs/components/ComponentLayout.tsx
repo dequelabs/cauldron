84cb640c19f4b05adb87cdd4b4a36fee0b1ab6de
Jason <jason@scurker.com>
2023-03-10 21:49:59 -0600

    pseudo active toc

--- docs/components/ComponentLayout.tsx
+++ docs/components/ComponentLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Offscreen, Link, Icon, Panel } from '@deque/cauldron-react';
 import slugify from '../utils/slugify';
 import './ComponentLayout.css';
@@ -15,6 +15,7 @@ interface ComponentLayoutProps extends React.HTMLAttributes<HTMLDivElement> {
   source?: string;
   deprecated?: boolean;
   toc: Heading[];
+  children: React.ReactNode;
 }
 
 function HeadingsToTOC(heading: Heading) {
@@ -37,17 +38,66 @@ export default function ComponentLayout({
   toc = [],
   deprecated = false
 }: ComponentLayoutProps) {
+  const containerRef = useRef<HTMLElement>(null);
+  const tocRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const container = document.querySelector('.Component');
+    const headings: Element[] = Array.from(
+      document.querySelectorAll('h2,h3,h4,h5,h6')
+    ).filter(el => !!el.id && containerRef.current.contains(el));
+    const anchors: HTMLLinkElement[] = Array.from(
+      tocRef.current.querySelectorAll('a[href]')
+    );
+
+    // Set the first element as active
+    anchors[0].classList.toggle('toc--active');
+
+    const handleIntersection: IntersectionObserverCallback = ([entry]) => {
+      const { target, isIntersecting, intersectionRect, rootBounds } = entry;
+      const clearActive = () =>
+        anchors.forEach(el => el.classList.remove('toc--active'));
+
+      if (
+        !isIntersecting &&
+        intersectionRect.top <= (rootBounds?.top as number)
+      ) {
+        clearActive();
+        const id = target.getAttribute('id');
+        anchors
+          .find(el => el.getAttribute('href')?.substr(1) === id)
+          ?.classList.toggle('toc--active');
+      } else {
+        // handle when element scrolls up
+      }
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: document.querySelector('.Layout'),
+      rootMargin: '0px',
+      threshold: 1.0
+    });
+
+    headings.forEach(el => observer.observe(el));
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <section aria-labelledby="on-this-page">
         <div>
           <h2 id="on-this-page">On This Page</h2>
           <nav>
-            <ul className="toc">{toc.map(HeadingsToTOC)}</ul>
+            <ul className="toc" ref={tocRef}>
+              {toc.map(HeadingsToTOC)}
+            </ul>
           </nav>
         </div>
       </section>
-      <div className="Component">
+      <div className="Component" ref={containerRef}>
         <h1 id={slugify(title)}>{title}</h1>
         {description && <p>{description}</p>}
 
