85ca0ecb240fa22688d90cc7a731187d91e7cb8f
Trevor Huey <trevor.huey@deque.com>
2023-03-20 13:06:38 -0500

    comment

--- packages/react/src/propTypes.ts
+++ packages/react/src/propTypes.ts
@@ -1,5 +1,9 @@
 import PropTypes from 'prop-types';
 
+/**
+ * This prop type is meant to ensure that a prop can actually be rendered as content.
+ * It should match the ContentNode type in types.ts
+ */
 export const contentNode = PropTypes.oneOfType([
   PropTypes.string,
   PropTypes.number,
