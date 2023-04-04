4c24053c9cf60cc21d42e7851025b3fe85952555
Jason <jason@scurker.com>
2023-04-04 16:25:29 -0500

    chore: remove release-please, use standard-version for now

--- RELEASING.md
+++ RELEASING.md
@@ -1,9 +1,36 @@
 # Releasing (for maintainers only)
 
-To create a new release of @deque/cauldron-react and @deque/cauldron-styles, follow the steps below.
+To create a new release of `@deque/cauldron-react` and `@deque/cauldron-styles`, follow the steps below.
 
-1. Navigate to [cauldron pull requests](https://github.com/dequelabs/cauldron/pulls?q=is%3Apr+is%3Aopen+label%3A%22autorelease%3A+pending%22) to see if there are any open PRs tagged as `autorelease: pending`.
-2. Review the changelog / commits
-3. If everything looks good, merge the PR into `master`
+## Creating a New Release
+
+```bash
+# Ensure you have the latest `develop` code.
+git checkout develop
+git pull origin develop
+git fetch --tags
+yarn run release
+```
+
+This will automagically create the release PR in a new browser window.
+
+Once the release has been reviewed, merge the release branch into the `master` branch.
+
+## Patch Release
+
+If a patch release is needed with only a few cherry-picked changes, follow the steps below:
+
+```bash
+$ git checkout develop
+$ git pull origin develop
+$ git checkout -b <name of branch>
+$ git cherry-pick <git hashes>
+$ git fetch --tags
+$ yarn run release
+```
+
+This will automagically create the release PR in a new browser window.
+
+Once the patch release has been reviewed, merge the release branch into the `master` branch.
 
 _NOTE: Do **not** `npm publish` yourself. CI will do this for you._
---
+++ scripts/release.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+throw() {
+  echo "$@" 1>&2
+  exit 1
+}
+
+branch_name=
+version=
+changelog=
+tag_prefix=
+message=
+branch_name="release-staging"
+version=$(node -p 'require("./package.json").version')
+changelog=CHANGELOG.md
+tag_prefix=
+message="chore(cauldron): Release {{currentTag}}"
+
+echo "Creating release staging branch: $branch_name"
+git checkout -b "$branch_name"
+
+# Run release stuff (update package.json#version, changelog, etc.)
+npx standard-version \
+  --releaseCommitMessageFormat="$message" \
+  --infile="$changelog" \
+  --commit-all \
+  --skip.tag true \
+  "${@:2}" # allow runtime args to be passed into standard-version
+
+# Get the new version number
+new_version=$(node -p 'require("./package.json").version')
+
+# Set the remote branch with the release version
+release_branch="release-v$new_version"
+
+git branch -M $release_branch
+
+if git ls-remote --exit-code origin "$release_branch" > /dev/null 2>&1; then
+  echo "Remote branch $release_branch already exists. Aborting."
+  exit 1
+fi
+
+git push origin $release_branch
+
+# Get the additions to the changelog as the commit body and generate the PR url
+uri_encoded_commit_body=$(git show --no-color --no-prefix --output-indicator-new=! AXE_CHANGELOG.md | egrep '^!' | awk -F'^[!]' '{print $2}' | sed -e 's/\n/$0A/g' | node -p 'encodeURIComponent(require("fs").readFileSync(0))')
+uri_encoded_message=$(git show --no-patch --format=%s | node -p 'encodeURIComponent(require("fs").readFileSync(0))')
+pr_url="https://github.com/dequelabs/cauldron/compare/master...$release_branch?title=$uri_encoded_message&body=$uri_encoded_commit_body"
+
+echo "\nOpening $pr_url in your browser."
+open -n $pr_url
