51959acc6c8e322812bf0c0f80e3f8b3ad24b8c4
Tom Busillo <tbusillo@users.noreply.github.com>
2023-07-31 14:09:32 -0400

    chore(docs,test): update tests and cleanup docs

--- packages/react/__tests__/src/components/Notice/index.js
+++ packages/react/__tests__/src/components/Notice/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { setImmediate } from 'timers/promises';
-import { mount, shallow } from 'enzyme';
-import sinon from 'sinon';
+import { mount } from 'enzyme';
 import Notice from 'src/components/Notice';
 import axe from '../../../axe';
 
@@ -13,9 +11,13 @@ test('handles rendering without errors', done => {
 
 test('should render with defaults when no props passed in', async () => {
   const wrapper = mount(<Notice>child</Notice>);
-  console.log(wrapper.debug());
+
   expect(wrapper.find('.Notice').length).toBe(1);
-  expect(wrapper.find('.Notice__icon').length).toBe(0);
+});
+test('should render the correct default icon for a given `type`', async () => {
+  const cautionNotice = mount(<Notice type="caution" title="Boom!" />);
+
+  expect(cautionNotice.find('Icon').prop('type')).toBe('caution');
 });
 
 test('should return no axe violations', async () => {
@@ -61,26 +63,22 @@ test('should fallback to the `info-circle` icon when an invalid icon type is pas
   expect(wrapper.find('Icon').prop('type')).toBe('info-circle');
 });
 
-test('should render with the correct icon when a valid icon type is passed in', async () => {
-  const wrapper = mount(
-    <Notice type="info" icon="bolt" title="foo">
-      Dynamo!
-    </Notice>
-  );
+test('should render with the correct icon when a valid icon `type` string is passed in', async () => {
+  const wrapper = mount(<Notice type="info" icon="bolt" title="Dynamo!" />);
 
   expect(wrapper.find('Notice').length).toBe(1);
   expect(wrapper.find('Icon').prop('type')).toBe('bolt');
   expect(wrapper.find('.Icon--bolt').length).toBe(1);
 });
 
-test('should render only a title when no children are passed in', async () => {
+test('should render only a `title` when no children are passed in', async () => {
   const wrapper = mount(<Notice title="foo" />);
 
   expect(wrapper.find('Notice').length).toBe(1);
   expect(wrapper.find('.Notice__title').contains('foo')).toBeTruthy();
 });
 
-test('`title` prop should allow for any valid React node', async () => {
+test('`title` prop should allow for any valid ReactNode element', async () => {
   const wrapper = mount(
     <Notice title={<h2>foo</h2>}>
       <span>bar</span>
@@ -88,11 +86,10 @@ test('`title` prop should allow for any valid React node', async () => {
   );
 
   expect(wrapper.find('Notice').length).toBe(1);
-  console.log(wrapper.debug());
   expect(wrapper.contains(<h2>foo</h2>)).toBeTruthy();
 });
 
-test('should allow rendering with children, but no title', () => {
+test('should allow rendering with children, but no title', async () => {
   const wrapper = mount(
     <Notice>
       <span>bar</span>
@@ -103,4 +100,12 @@ test('should allow rendering with children, but no title', () => {
   expect(wrapper.find('Icon').length).toBe(0);
   expect(wrapper.find('span').length).toBe(1);
   expect(wrapper.find('span').contains('bar')).toBeTruthy();
+});
+
+test('should allow a ref to be forwarded', async () => {
+  const ref = React.createRef();
+  const wrapper = mount(<Notice ref={ref} />);
+
+  expect(wrapper.find('Notice').length).toBe(1);
+  expect(ref.current).toBeTruthy();
 });
