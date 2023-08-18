5bae546c62f41b175d5eb10615c0daac10aebdc2
Jason <jason@scurker.com>
2023-08-18 10:57:04 -0500

    check for property descriptor

--- packages/react/__tests__/setupTests.js
+++ packages/react/__tests__/setupTests.js
@@ -4,10 +4,14 @@ import 'jest-axe/extend-expect';
 
 configure({ adapter: new Adapter() });
 
-// JSDOM doesn't fully support innerText, but we can fall back to
-// using textContent for now until this gets patched
-Object.defineProperty(window.HTMLElement.prototype, 'innerText', {
-  get() {
-    return this.textContent;
-  },
-});
+if (
+  !Object.getOwnPropertyDescriptor(window.HTMLElement.prototype, 'innerText')
+) {
+  // JSDOM doesn't fully support innerText, but we can fall back to
+  // using textContent for now until this gets patched
+  Object.defineProperty(window.HTMLElement.prototype, 'innerText', {
+    get() {
+      return this.textContent;
+    },
+  });
+}
