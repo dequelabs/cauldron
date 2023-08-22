80309fc0f6c797580cbfbb50a60e1f465ce8952c
Jason <jason@scurker.com>
2023-08-22 15:34:35 -0500

    add search to icon docs

--- docs/components/IconGrid.tsx
+++ docs/components/IconGrid.tsx
@@ -1,18 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { Icon, iconTypes } from '@deque/cauldron-react';
+import React, { useState, useRef } from 'react';
+import {
+  Icon,
+  iconTypes,
+  TextField,
+  Panel,
+  Button
+} from '@deque/cauldron-react';
 import './IconGrid.css';
 
 export default function IconGrid() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [searchValue, setSearchValue] = useState('');
+  const filteredTypes = searchValue.trim().length
+    ? iconTypes.filter((type: string) =>
+        type.includes(searchValue.trim().toLowerCase())
+      )
+    : iconTypes;
+
+  const handleChange = (value: string) => {
+    setSearchValue(value);
+  };
+
+  const handleClick = () => {
+    setSearchValue('');
+    inputRef.current?.focus();
+  };
+
   return (
-    <div className="IconGrid">
-      <ul>
-        {iconTypes.map((type: string) => (
-          <li className="Panel" key={type}>
-            <Icon type={type} />
-            <div className="IconGrid__label">{type}</div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <TextField
+        fieldRef={inputRef}
+        label="Filter Icons"
+        type="search"
+        placeholder="search for icons by type..."
+        value={searchValue}
+        onChange={handleChange}
+      />
+      <div className="IconGrid">
+        {filteredTypes.length ? (
+          <ul>
+            {filteredTypes.map((type: string) => (
+              <li className="Panel" key={type}>
+                <Icon type={type} />
+                <div className="IconGrid__label">{type}</div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <Panel className="IconGrid__NoResults">
+            <p role="status">
+              No icon types found matching <strong>"{searchValue}"</strong>.
+            </p>
+            <Button variant="secondary" onClick={handleClick}>
+              Clear Search
+            </Button>
+          </Panel>
+        )}
+      </div>
+    </>
   );
 }
