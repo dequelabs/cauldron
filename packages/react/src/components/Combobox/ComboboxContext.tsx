24bdf938fd73fe9811cb8e210523f25783ae181c
Jason <jason@scurker.com>
2023-08-29 14:23:56 -0500

    lint

--- packages/react/src/components/Combobox/ComboboxContext.tsx
+++ packages/react/src/components/Combobox/ComboboxContext.tsx
@@ -31,7 +31,7 @@ const ComboboxContext = createContext<ComboboxContext>({
   selectedValue: undefined,
   matches: true,
   matchingOptions: new Map(),
-  setMatchingOptions: () => {}
+  setMatchingOptions: () => null
 });
 
 function ComboboxProvider({
