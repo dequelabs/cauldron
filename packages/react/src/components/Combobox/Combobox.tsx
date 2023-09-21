ffd8071d28fb24dcc559f195628ed9f709f9ee1b
Jason <jason@scurker.com>
2023-09-21 12:30:40 -0500

    use role alert

--- packages/react/src/components/Combobox/Combobox.tsx
+++ packages/react/src/components/Combobox/Combobox.tsx
@@ -65,20 +65,9 @@ const ComboboxNoResults = ({
 }: {
   children?: React.ReactNode;
 }): JSX.Element => {
-  const [liveText, setLiveText] = useState<React.ReactNode>('');
-
-  useEffect(() => {
-    // Note: NVDA will not announce live regions unless initially mounted,
-    // we work around this by arbitrarily updating the live region text
-    // on the next animation frame.
-    requestAnimationFrame(() => {
-      setLiveText(children || 'No results found.');
-    });
-  }, []);
-
   return (
-    <div className="ComboboxListbox__empty" aria-live="polite">
-      {liveText}
+    <div className="ComboboxListbox__empty" role="alert" aria-live="polite">
+      {children || 'No results found.'}
     </div>
   );
 };
