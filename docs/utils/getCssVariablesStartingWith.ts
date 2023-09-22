27a6687de66f3b6177fc4b6291465353a0bd1c98
orest-s <orest.hlodan@deque.com>
2023-09-22 20:15:56 +0300

    feat: added utility function to get variables by the start name

---
+++ docs/utils/getCssVariablesStartingWith.ts
@@ -0,0 +1,35 @@
+export function getCssVariablesStartingWith(prefix: string) {
+  const cssVariables: { [key: string]: string } = {};
+
+  // Iterate through all style sheets in the document
+  for (const styleSheet of Array.from(document.styleSheets)) {
+    try {
+      if (styleSheet.cssRules) {
+        // Iterate through the rules in the style sheet
+        for (const rule of Array.from(styleSheet.cssRules)) {
+          if (rule instanceof CSSStyleRule) {
+            const style = rule.style;
+
+            // Iterate through the style properties of each rule
+            for (let i = 0; i < style.length; i++) {
+              const propertyName = style[i];
+              const propertyValue = style.getPropertyValue(propertyName);
+
+              // Check if the property is a CSS variable and starts with the prefix
+              if (
+                propertyName.startsWith('--') &&
+                propertyName.startsWith(prefix)
+              ) {
+                cssVariables[propertyName] = propertyValue;
+              }
+            }
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Error accessing stylesheet:', error);
+    }
+  }
+
+  return cssVariables;
+}
