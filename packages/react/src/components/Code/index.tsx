7c6477d2ec5cb5540e84ba4c07c9810cee03e2f0
Jason <jason@scurker.com>
2023-09-25 12:43:44 -0500

    fix(react): prevent esm modules from appearing in cjs dist

--- packages/react/src/components/Code/index.tsx
+++ packages/react/src/components/Code/index.tsx
@@ -1,12 +1,12 @@
 import React, { useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { SyntaxHighlighterProps } from 'react-syntax-highlighter';
-import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/light';
+import SyntaxHighlighter from 'react-syntax-highlighter/dist/cjs/light';
 import classNames from 'classnames';
-import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
-import css from 'react-syntax-highlighter/dist/esm/languages/hljs/css';
-import xml from 'react-syntax-highlighter/dist/esm/languages/hljs/xml';
-import yaml from 'react-syntax-highlighter/dist/esm/languages/hljs/yaml';
+import js from 'react-syntax-highlighter/dist/cjs/languages/hljs/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/hljs/css';
+import xml from 'react-syntax-highlighter/dist/cjs/languages/hljs/xml';
+import yaml from 'react-syntax-highlighter/dist/cjs/languages/hljs/yaml';
 
 SyntaxHighlighter.registerLanguage('javascript', js);
 SyntaxHighlighter.registerLanguage('css', css);
@@ -14,9 +14,8 @@ SyntaxHighlighter.registerLanguage('html', xml);
 SyntaxHighlighter.registerLanguage('yaml', yaml);
 
 // HACK: This is a workaround for a bug in react-syntax-highlighter's types.
-const Highlighter = SyntaxHighlighter as React.ComponentType<
-  SyntaxHighlighterProps
->;
+const Highlighter =
+  SyntaxHighlighter as React.ComponentType<SyntaxHighlighterProps>;
 
 type Props = {
   children: string;
