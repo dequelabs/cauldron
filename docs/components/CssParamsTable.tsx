cb200b1346118c4fb710d3268adeab6cc294a907
orest-s <orest.hlodan@deque.com>
2023-10-09 17:19:56 +0300

    feat: improved CssParamsTable, using renderExample and formatName functions

--- docs/components/CssParamsTable.tsx
+++ docs/components/CssParamsTable.tsx
@@ -11,7 +11,17 @@ import {
 
 import { getCssVariablesStartingWith } from '../utils/getCssVariablesStartingWith';
 
-const CssParamsTable = ({ param = '--text-size' }) => {
+interface CssParamsTableProps {
+  param?: string;
+  renderExample?: (name: string, value: string) => JSX.Element;
+  formatName?: (name: string) => string;
+}
+
+const CssParamsTable = ({
+  param = '--text-size',
+  renderExample,
+  formatName = (name: string) => name
+}: CssParamsTableProps) => {
   const textSizes = getCssVariablesStartingWith(param);
 
   return (
@@ -21,14 +31,18 @@ const CssParamsTable = ({ param = '--text-size' }) => {
           <TableHeader scope="col">Name</TableHeader>
           <TableHeader scope="col">Value</TableHeader>
           <TableHeader scope="col">Description</TableHeader>
+          {renderExample && <TableHeader scope="col">Example</TableHeader>}
         </TableRow>
       </TableHead>
       <TableBody>
         {Object.entries(textSizes).map(([name, value]) => (
           <TableRow>
-            <TableCell>{name}</TableCell>
+            <TableCell>{formatName(name)}</TableCell>
             <TableCell>{value}</TableCell>
             <TableCell>-</TableCell>
+            {renderExample && (
+              <TableCell>{renderExample(name, value)}</TableCell>
+            )}
           </TableRow>
         ))}
       </TableBody>
