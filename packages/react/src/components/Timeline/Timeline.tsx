38a361382791d601f99368426e4df4445171e2c4
Jason <jason@scurker.com>
2024-01-26 09:44:05 -0600

    padding

--- packages/react/src/components/Timeline/Timeline.tsx
+++ packages/react/src/components/Timeline/Timeline.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
 
-type TimelineProps = React.HTMLAttributes<HTMLOListElement>;
+interface TimelineProps extends React.HTMLAttributes<HTMLOListElement> {}
 
 export default function Timeline({
   className,
