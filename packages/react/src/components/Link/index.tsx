5d92f1e2b49f6a5ce814de4c996c285c79d4fab4
Jason <jason@scurker.com>
2024-02-23 09:30:24 -0600

    feat(react): add ref support to Link (#1357)

--- packages/react/src/components/Link/index.tsx
+++ packages/react/src/components/Link/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import classNames from 'classnames';
 
 export interface LinkProps
@@ -8,26 +8,26 @@ export interface LinkProps
   thin?: boolean;
 }
 
-const Link = ({
-  children,
-  linkRef,
-  className,
-  variant,
-  thin,
-  ...other
-}: LinkProps) => (
-  <a
-    ref={linkRef}
-    className={classNames(className, {
-      Link: !variant,
-      'Button--primary': variant === 'button',
-      'Button--secondary': variant === 'button-secondary',
-      'Button--thin': thin
-    })}
-    {...other}
-  >
-    {children}
-  </a>
+const Link = forwardRef<HTMLAnchorElement, LinkProps>(
+  (
+    { children, linkRef, className, variant, thin, ...other }: LinkProps,
+    ref
+  ) => {
+    return (
+      <a
+        ref={ref || linkRef}
+        className={classNames(className, {
+          Link: !variant,
+          'Button--primary': variant === 'button',
+          'Button--secondary': variant === 'button-secondary',
+          'Button--thin': thin
+        })}
+        {...other}
+      >
+        {children}
+      </a>
+    );
+  }
 );
 
 Link.displayName = 'Link';
