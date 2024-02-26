091e63da661190f5cf4808cc26abbec4aa72ef22
lsprr <16653744+lsprr@users.noreply.github.com>
2024-02-26 13:41:34 -0500

    Minor correction

--- packages/react/src/components/Line/index.test.tsx
+++ packages/react/src/components/Line/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Line from '../../../src/components/Line';
+import Line from './index';
 import axe from '../../axe';
 
 test('passes classNames through', () => {
