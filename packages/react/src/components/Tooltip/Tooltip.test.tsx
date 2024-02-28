078941ac0571c1bdfae10009911d6c947bb9b3a1
Jason <jason@scurker.com>
2024-02-28 14:55:07 -0600

    test: update Tooltip tests to use react testing library

---
+++ packages/react/src/components/Tooltip/Tooltip.test.tsx
@@ -0,0 +1,172 @@
+import React, { createRef } from 'react';
+import { setTimeout } from 'timers/promises';
+import { render, screen, fireEvent, getByRole } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { spy } from 'sinon';
+import { axe } from 'jest-axe';
+import Tooltip, { TooltipHead, TooltipContent } from './';
+
+type SetOptional<T, Keys extends keyof T> = Pick<Partial<T>, Keys> &
+  Omit<T, Keys>;
+interface RenderTooltipProps {
+  buttonProps?: React.HTMLAttributes<HTMLButtonElement>;
+  tooltipProps?: SetOptional<
+    Omit<React.ComponentProps<typeof Tooltip>, 'target'>,
+    'children'
+  >;
+}
+
+const renderTooltip = ({
+  buttonProps = {},
+  tooltipProps = {}
+}: RenderTooltipProps = {}) => {
+  const ref = createRef<HTMLButtonElement>();
+  const { children, ...props } = tooltipProps;
+  return render(
+    <>
+      <button ref={ref} {...buttonProps}>
+        button
+      </button>
+      <Tooltip target={ref} show {...props}>
+        {children ?? 'Hello Tooltip'}
+      </Tooltip>
+    </>
+  );
+};
+
+test('should render tooltip', () => {
+  renderTooltip();
+  expect(
+    screen.getByRole('tooltip', { name: 'Hello Tooltip' })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole('button', { name: 'button' })
+  ).toHaveAccessibleDescription('Hello Tooltip');
+});
+
+test('should auto generate ids', () => {
+  renderTooltip();
+  const button = screen.getByRole('button');
+  const tooltip = screen.getByRole('tooltip');
+  expect(tooltip.getAttribute('id')).toBeTruthy();
+  expect(tooltip.getAttribute('id')).toEqual(
+    button.getAttribute('aria-describedby')
+  );
+});
+
+test('should not overwrite user provided ids', () => {
+  const buttonProps = { [`aria-describedby`]: 'foo tooltipid' };
+  const tooltipProps = { id: 'tooltipid' };
+  renderTooltip({ buttonProps, tooltipProps });
+  expect(screen.getByRole('tooltip').getAttribute('id')).toEqual('tooltipid');
+  expect(screen.getByRole('button').getAttribute('aria-describedby')).toEqual(
+    'foo tooltipid'
+  );
+});
+
+test('should show tooltip on target element focus', async () => {
+  // @ts-expect-error force show to override existing show value
+  renderTooltip({ tooltipProps: { show: null } });
+  expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+  await fireEvent.focusIn(screen.getByRole('button'));
+  expect(screen.queryByRole('tooltip')).toBeInTheDocument();
+});
+
+test('should show tooltip on target element hover', async () => {
+  // @ts-expect-error force show to override existing show value
+  renderTooltip({ tooltipProps: { show: null } });
+  expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+  await fireEvent.mouseEnter(screen.getByRole('button'));
+  expect(screen.queryByRole('tooltip')).toBeInTheDocument();
+});
+
+test('should hide tooltip on target element blur', async () => {
+  // @ts-expect-error force show to override existing show value
+  renderTooltip({ tooltipProps: { show: null } });
+  expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+  await fireEvent.focusIn(screen.getByRole('button'));
+  expect(screen.queryByRole('tooltip')).toBeInTheDocument();
+  await fireEvent.focusOut(screen.getByRole('button'));
+  // Note: Tooltip does not immediately hide, but is delayed by 100ms
+  expect(screen.queryByRole('tooltip')).toBeInTheDocument();
+  await setTimeout(100);
+  expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+});
+
+test('should hide tooltip on escape keypress', async () => {
+  const user = userEvent.setup();
+  // @ts-expect-error force show to override existing show value
+  renderTooltip({ tooltipProps: { show: null } });
+  expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+  await fireEvent.focusIn(screen.getByRole('button'));
+  expect(screen.queryByRole('tooltip')).toBeInTheDocument();
+  await userEvent.keyboard('{Escape}');
+  expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+});
+
+test('should fire the "cauldron:tooltip:show" custom event when tooltip is shown', async () => {
+  const show = spy();
+  renderTooltip();
+
+  const button = screen.getByRole('button');
+  button.addEventListener('cauldron:tooltip:show', show);
+  await fireEvent.focusIn(screen.getByRole('button'));
+
+  expect(show.calledOnce).toBeTruthy();
+});
+
+test('should fire the "cauldron:tooltip:hide" custom event when tooltip is hidden', async () => {
+  const hide = spy();
+  renderTooltip();
+
+  const button = screen.getByRole('button');
+  button.addEventListener('cauldron:tooltip:hide', hide);
+  await fireEvent.focusOut(screen.getByRole('button'));
+  // Note: Tooltip does not immediately hide, but is delayed by 100ms
+  await setTimeout(100);
+
+  expect(hide.calledOnce).toBeTruthy();
+});
+
+test('should support className prop', () => {
+  renderTooltip({ tooltipProps: { className: 'bananas' } });
+  expect(screen.getByRole('tooltip')).toHaveClass('Tooltip', 'bananas');
+});
+
+test('should support portal prop', () => {
+  const portal = document.createElement('div');
+  renderTooltip({ tooltipProps: { portal } });
+  expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+  expect(getByRole(portal, 'tooltip')).toBeTruthy();
+});
+
+test('should support association prop', () => {
+  renderTooltip({ tooltipProps: { association: 'aria-labelledby' } });
+  expect(screen.queryByRole('button')).toHaveAccessibleName('Hello Tooltip');
+});
+
+test('should return no axe violations with default variant', async () => {
+  const { container } = renderTooltip();
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+});
+
+test('should return no axe violations with info variant', async () => {
+  const { container } = renderTooltip({ tooltipProps: { variant: 'info' } });
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+});
+
+test('should return no axe violations with big variant', async () => {
+  const children = (
+    <>
+      <TooltipHead>Head</TooltipHead>
+      <TooltipContent>Content</TooltipContent>
+    </>
+  );
+  const { container } = renderTooltip({
+    tooltipProps: { variant: 'big', children }
+  });
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+});
