6831366affc6b4858fe9b60f77e348057e7463b6
Yahya Hafez <yhafez3@gmail.com>
2024-03-01 14:39:31 -0800

    chore: address pr reviews

--- packages/react/src/components/SearchField/index.tsx
+++ packages/react/src/components/SearchField/index.tsx
@@ -3,20 +3,19 @@ import React, {
   useRef,
   ChangeEvent,
   InputHTMLAttributes,
-  ReactNode,
-  forwardRef,
-  ForwardRefRenderFunction
+  forwardRef
 } from 'react';
 import classNames from 'classnames';
 import { useId } from 'react-id-generator';
 
+import type { ContentNode } from '../../types';
 import TextFieldWrapper from '../internal/TextFieldWrapper';
 import Offscreen from '../Offscreen';
 import Icon from '../Icon';
 
 interface SearchFieldProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
-  label: ReactNode;
+  label: ContentNode;
   value?: string;
   defaultValue?: string;
   onChange?: (value: string, e: ChangeEvent<HTMLInputElement>) => void;
@@ -24,94 +23,88 @@ interface SearchFieldProps
   isForm?: boolean;
 }
 
-const SearchFieldComponent: ForwardRefRenderFunction<
-  HTMLInputElement,
-  SearchFieldProps
-> = (
-  {
-    label,
-    defaultValue = '',
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    onChange = () => {},
-    hideLabel = false,
-    placeholder = 'Search...',
-    isForm = true,
-    id: propId,
-    value: propValue,
-    ...otherProps
-  },
-  ref
-) => {
-  const [value, setValue] = useState<string | undefined>(
-    typeof propValue !== 'undefined' ? propValue : defaultValue
-  );
+const SearchField = forwardRef<HTMLInputElement, SearchFieldProps>(
+  (
+    {
+      label,
+      defaultValue = '',
+      onChange,
+      hideLabel = false,
+      placeholder = 'Search...',
+      isForm = true,
+      id: propId,
+      value: propValue,
+      ...otherProps
+    }: SearchFieldProps,
+    ref
+  ) => {
+    const [value, setValue] = useState<string | undefined>(
+      typeof propValue !== 'undefined' ? propValue : defaultValue
+    );
 
-  const [id] = propId ? [propId] : useId(1, 'search-field');
-  const inputId = useRef(id).current;
+    const [id] = propId ? [propId] : useId(1, 'search-field');
+    const inputId = useRef(id).current;
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newValue = e.target.value;
-    if (typeof onChange === 'function') {
-      onChange(newValue, e);
-    }
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value;
+      if (typeof onChange === 'function') {
+        onChange(newValue, e);
+      }
 
-    const isControlled = typeof propValue !== 'undefined';
+      const isControlled = typeof propValue !== 'undefined';
 
-    if (isControlled) {
-      return;
-    }
+      if (isControlled) {
+        return;
+      }
 
-    setValue(newValue);
-  };
+      setValue(newValue);
+    };
 
-  const Field = isForm ? 'form' : 'div';
+    const Field = isForm ? 'form' : 'div';
 
-  return (
-    <Field
-      role={isForm ? 'search' : undefined}
-      className="SearchField"
-      aria-labelledby={`${inputId}-label`}
-    >
-      {hideLabel ? (
-        <Offscreen>
-          <label htmlFor={inputId} id={`${inputId}-label`}>
+    return (
+      <Field
+        role={isForm ? 'search' : undefined}
+        className="SearchField"
+        aria-labelledby={`${inputId}-label`}
+      >
+        {hideLabel ? (
+          <Offscreen>
+            <label htmlFor={inputId} id={`${inputId}-label`}>
+              {label}
+            </label>
+          </Offscreen>
+        ) : (
+          <label
+            className="Field__label"
+            htmlFor={inputId}
+            id={`${inputId}-label`}
+          >
             {label}
           </label>
-        </Offscreen>
-      ) : (
-        <label
-          className="Field__label"
-          htmlFor={inputId}
-          id={`${inputId}-label`}
+        )}
+        <TextFieldWrapper
+          className={classNames({
+            'TextFieldWrapper--disabled': otherProps.disabled
+          })}
         >
-          {label}
-        </label>
-      )}
-      <TextFieldWrapper
-        className={classNames({
-          'TextFieldWrapper--disabled': otherProps.disabled
-        })}
-      >
-        <Icon
-          type="magnifying-glass"
-          aria-hidden="true"
-          className="Field__search-icon"
-        />
-        <input
-          id={inputId}
-          value={value}
-          onChange={handleChange}
-          placeholder={placeholder}
-          ref={ref}
-          {...otherProps}
-          className={classNames(otherProps.className, 'Field__text-input')}
-          type="search"
-        />
-      </TextFieldWrapper>
-    </Field>
-  );
-};
+          <Icon type="magnifying-glass" className="Field__search-icon" />
+          <input
+            id={inputId}
+            value={value}
+            onChange={handleChange}
+            placeholder={placeholder}
+            ref={ref}
+            {...otherProps}
+            className={classNames(otherProps.className, 'Field__text-input')}
+            type="search"
+          />
+        </TextFieldWrapper>
+      </Field>
+    );
+  }
+);
 
-const SearchField = forwardRef(SearchFieldComponent);
 SearchField.displayName = 'SearchField';
+
 export default SearchField;
