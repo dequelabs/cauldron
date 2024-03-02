be6a2f1fe7b19271209e8a3788c498819d1d10fb
lsprr <16653744+lsprr@users.noreply.github.com>
2024-03-01 21:29:46 -0500

    Minor corrections

--- packages/react/src/components/Tabs/tab.test.tsx
+++ packages/react/src/components/Tabs/tab.test.tsx
@@ -4,7 +4,7 @@ import Tabs, { Tab } from './';
 
 const ariaLabel = 'I am a label';
 
-test('should render tab with a paragraph correctly', () => {
+test('should render tab with a paragraph correctly', async () => {
   const TabswithRef = () => {
     const ref = useRef(null);
     return (
@@ -15,7 +15,7 @@ test('should render tab with a paragraph correctly', () => {
   };
 
   render(<TabswithRef />);
-  waitFor(() => {
+  await waitFor(() => {
     expect(screen.getAllByRole('tab')).toHaveLength(1);
   });
 });
@@ -33,28 +33,28 @@ test('should pass classNames through', async () => {
   };
 
   render(<TabswithRef />);
-  waitFor(() => {
+  await waitFor(() => {
     expect(screen.getAllByRole('tab')).toHaveLength(2);
     expect(screen.getAllByRole('tab')[0]).toHaveClass('Tab--active');
     expect(screen.getAllByRole('tab')[0]).toBeInTheDocument();
     expect(screen.getAllByRole('tab')[1]).not.toHaveClass('Tab--active');
-    expect(screen.getAllByRole('tab')[1]).not.toHaveLength(0);
+    expect(screen.getAllByRole('tab')[1]).toBeInTheDocument();
   });
 });
 
-test('should pass id through', () => {
+test('should pass id through', async () => {
   const TabswithRef = () => {
-    const tabPanel1 = useRef(null);
+    const ref = useRef(null);
     return (
-      <Tab target={tabPanel1} id={'I am a tabId'}>
+      <Tab target={ref} id={'I am a tabId'}>
         option 1
       </Tab>
     );
   };
 
   render(<TabswithRef />);
-  waitFor(() => {
-    expect(screen.queryByTestId('I am a tabId')).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.getByRole('tab')).toHaveAttribute('id', 'I am a tabId');
   });
 });
 
@@ -73,7 +73,7 @@ test('should pass aria-selected through and render aria-selected properly', asyn
   };
 
   render(<TabswithRef />);
-  waitFor(() => {
+  await waitFor(() => {
     expect(screen.getAllByRole('tab')[0]).toHaveAttribute(
       'aria-selected',
       'false'
