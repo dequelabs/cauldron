9cb5a1989c2468563d2c326b4d1c2982653e27b2
Jason <jason@scurker.com>
2024-03-06 14:31:53 -0600

    words

--- packages/react/src/utils/idRefs.test.ts
+++ packages/react/src/utils/idRefs.test.ts
@@ -1,6 +1,6 @@
 import { default as idRefs, addIdRef, removeIdRef, hasIdRef } from './idRefs';
 
-test('should provide ref ids as a set', () => {
+test('should provide id refs as a set', () => {
   const ids = idRefs('one two three one four five');
   expect(ids.size).toEqual(5);
   expect([...ids].join(' ')).toEqual('one two three four five');
@@ -17,23 +17,23 @@ test('should handle extra spaces', () => {
   expect([...ids].join(' ')).toEqual('one two');
 });
 
-test('should add refId', () => {
+test('should add id ref', () => {
   expect(addIdRef('one two', 'three')).toEqual('one two three');
 });
 
-test('should not add existing refId', () => {
+test('should not add existing id ref', () => {
   expect(addIdRef('one two three', 'two')).toEqual('one two three');
 });
 
-test('should remove refId', () => {
+test('should remove id ref', () => {
   expect(removeIdRef('one two three', 'two')).toEqual('one three');
 });
 
-test('should return true when refId contains id', () => {
+test('should return true when id ref contains id', () => {
   expect(hasIdRef('one two three', 'two')).toBeTruthy();
 });
 
-test('should return false when refId does not contain id', () => {
+test('should return false when id ref does not contain id', () => {
   expect(hasIdRef('one three', 'two')).toBeFalsy();
 });
 
