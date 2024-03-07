2b42d12cc57c9df7383dba44b6f89de46f017b99
lsprr <16653744+lsprr@users.noreply.github.com>
2024-03-07 18:58:39 -0500

    Minor correction

--- packages/react/src/components/Tabs/tab.test.tsx
+++ packages/react/src/components/Tabs/tab.test.tsx
@@ -1,86 +1,70 @@
-import React, { useRef } from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import Tabs, { Tab } from './';
 
 const ariaLabel = 'I am a label';
 
 test('should render tab with a paragraph correctly', async () => {
-  const TabswithRef = () => {
-    const ref = useRef(null);
-    return (
-      <Tab target={ref} id={'I am a tabId'}>
-        <p>a simple paragraph</p>
-      </Tab>
-    );
-  };
+  const ref = React.createRef<HTMLDivElement>();
 
-  render(<TabswithRef />);
-  await waitFor(() => {
-    expect(screen.getAllByRole('tab')).toHaveLength(1);
-  });
+  render(
+    <Tab target={ref} id={'I am a tabId'}>
+      <p>a simple paragraph</p>
+    </Tab>
+  );
+
+  expect(screen.getAllByRole('tab')).toHaveLength(1);
 });
 
 test('should pass classNames through', async () => {
-  const TabswithRef = () => {
-    const ref = useRef(null);
-    const ref2 = useRef(null);
-    return (
-      <Tabs aria-label={ariaLabel}>
-        <Tab target={ref}>option 1</Tab>
-        <Tab target={ref2}>option 2</Tab>
-      </Tabs>
-    );
-  };
+  const ref = React.createRef<HTMLDivElement>();
+  const ref2 = React.createRef<HTMLDivElement>();
 
-  render(<TabswithRef />);
-  await waitFor(() => {
-    expect(screen.getAllByRole('tab')).toHaveLength(2);
-    expect(screen.getAllByRole('tab')[0]).toHaveClass('Tab--active');
-    expect(screen.getAllByRole('tab')[0]).toBeInTheDocument();
-    expect(screen.getAllByRole('tab')[1]).not.toHaveClass('Tab--active');
-    expect(screen.getAllByRole('tab')[1]).toBeInTheDocument();
-  });
+  render(
+    <Tabs aria-label={ariaLabel}>
+      <Tab target={ref}>option 1</Tab>
+      <Tab target={ref2}>option 2</Tab>
+    </Tabs>
+  );
+
+  expect(screen.getAllByRole('tab')).toHaveLength(2);
+  expect(screen.getAllByRole('tab')[0]).toHaveClass('Tab--active');
+  expect(screen.getAllByRole('tab')[0]).toBeInTheDocument();
+  expect(screen.getAllByRole('tab')[1]).not.toHaveClass('Tab--active');
+  expect(screen.getAllByRole('tab')[1]).toBeInTheDocument();
 });
 
 test('should pass id through', async () => {
-  const TabswithRef = () => {
-    const ref = useRef(null);
-    return (
-      <Tab target={ref} id={'I am a tabId'}>
-        option 1
-      </Tab>
-    );
-  };
+  const ref = React.createRef<HTMLDivElement>();
 
-  render(<TabswithRef />);
-  await waitFor(() => {
-    expect(screen.getByRole('tab')).toHaveAttribute('id', 'I am a tabId');
-  });
+  render(
+    <Tab target={ref} id={'I am a tabId'}>
+      option 1
+    </Tab>
+  );
+
+  expect(screen.getByRole('tab')).toHaveAttribute('id', 'I am a tabId');
 });
 
 test('should pass aria-selected through and render aria-selected properly', async () => {
-  const TabswithRef = () => {
-    const ref = useRef(null);
-    const ref2 = useRef(null);
-    return (
-      <>
-        <Tabs aria-label={ariaLabel} initialActiveIndex={1}>
-          <Tab target={ref}>option 1</Tab>
-          <Tab target={ref2}>option 2</Tab>
-        </Tabs>
-      </>
-    );
-  };
+  const ref = React.createRef<HTMLDivElement>();
+  const ref2 = React.createRef<HTMLDivElement>();
 
-  render(<TabswithRef />);
-  await waitFor(() => {
-    expect(screen.getAllByRole('tab')[0]).toHaveAttribute(
-      'aria-selected',
-      'false'
-    );
-    expect(screen.getAllByRole('tab')[1]).toHaveAttribute(
-      'aria-selected',
-      'true'
-    );
-  });
+  render(
+    <>
+      <Tabs aria-label={ariaLabel} initialActiveIndex={1}>
+        <Tab target={ref}>option 1</Tab>
+        <Tab target={ref2}>option 2</Tab>
+      </Tabs>
+    </>
+  );
+
+  expect(screen.getAllByRole('tab')[0]).toHaveAttribute(
+    'aria-selected',
+    'false'
+  );
+  expect(screen.getAllByRole('tab')[1]).toHaveAttribute(
+    'aria-selected',
+    'true'
+  );
 });
