4f939bba94c4aee97f8f7367450afa87d12cfd4f
lsprr <16653744+lsprr@users.noreply.github.com>
2024-03-18 14:24:18 -0400

    Refactor and fix issues based on feedback

--- packages/react/src/components/Alert/index.test.tsx
+++ packages/react/src/components/Alert/index.test.tsx
@@ -6,21 +6,41 @@ import axe from '../../axe';
 const defaults = { show: false, heading: { text: 'hi' } };
 
 test('should return null when passed a falsey "show" prop', () => {
+  render(<Alert {...defaults}>Test Alert</Alert>);
+
+  expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+});
+
+test('should pass classNames through', () => {
   render(
-    <Alert data-testid="alert" {...defaults}>
+    <Alert {...defaults} show className="baz">
       Test Alert
     </Alert>
   );
-  expect(screen.queryByTestId('alert')).toBeNull();
+
+  expect(screen.queryByRole('dialog')).toHaveClass('Alert', 'baz');
+});
+
+test('should pass ref prop through', () => {
+  const ref = React.createRef<HTMLDivElement>();
+
+  render(
+    <Alert {...defaults} show dialogRef={ref}>
+      Test Alert
+    </Alert>
+  );
+
+  expect(ref.current).toBeInTheDocument();
 });
 
 test('should show modal when passed a truthy "show" prop', () => {
   render(
-    <Alert data-testid="alert" {...defaults} show>
+    <Alert {...defaults} show>
       Test Alert
     </Alert>
   );
-  expect(screen.getByTestId('alert')).toBeInTheDocument();
+
+  expect(screen.queryByRole('dialog')).toBeInTheDocument();
 });
 
 test('should return no axe violations', async () => {
