1ffb8b1da264487dd67ed37ca378b7846bef93fb
Aravind <aravinda.kummetha@deque.com>
2024-04-15 17:10:09 +0530

    fixed build issue

--- docs/components/CopyToClipboardButton.tsx
+++ docs/components/CopyToClipboardButton.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useMemo } from 'react';
 import { IconButton, Toast } from '@deque/cauldron-react';
 
 interface CopyToClipboardButtonProps
