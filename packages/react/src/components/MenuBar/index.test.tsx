18378870146e1484dd4a19850d904eccec2c4a25
Denys Fedorov <denys.fedorov@Sombra-L501.local>
2024-06-03 17:16:52 +0300

    test: switch menubar tests to react testing library

---
+++ packages/react/src/components/MenuBar/index.test.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import axe from '../../axe';
+import MenuBar from '../MenuBar';
+import { TopBarItem } from '../TopBar';
+
+const [right, left] = [39, 37];
+const child = (
+  <TopBarItem>
+    <div />
+  </TopBarItem>
+);
+
+test('renders', () => {
+  render(<MenuBar>{child}</MenuBar>);
+  expect(screen.getByRole('menubar')).toBeTruthy();
+});
+
+test('supports falsy children', () => {
+  const { container } = render(
+    <MenuBar>
+      {child}
+      {false && child}
+    </MenuBar>
+  );
+  expect(container).toBeTruthy();
+  expect(container.querySelectorAll('div').length).toBe(1);
+});
+
+test('handles left arrow', async () => {
+  const { getAllByRole } = render(
+    <MenuBar>
+      <TopBarItem>test-1</TopBarItem>
+      <TopBarItem>test-2</TopBarItem>
+      <TopBarItem>test-3</TopBarItem>
+    </MenuBar>
+  );
+
+  const menuItems = getAllByRole('menuitem');
+  fireEvent.keyDown(menuItems[1], { key: 'ArrowLeft', keyCode: left });
+  await waitFor(() => {
+    expect(menuItems[0]).toHaveFocus();
+    expect(document.activeElement).toBe(menuItems[0]);
+  });
+
+  fireEvent.keyDown(menuItems[2], { key: 'ArrowLeft', keyCode: left });
+  await waitFor(() => {
+    expect(menuItems[1]).toHaveFocus();
+    expect(document.activeElement).toBe(menuItems[1]);
+  });
+});
+
+test('handles right arrow', async () => {
+  const { getAllByRole } = render(
+    <MenuBar>
+      <TopBarItem>test-1</TopBarItem>
+      <TopBarItem>test-2</TopBarItem>
+      <TopBarItem>test-3</TopBarItem>
+    </MenuBar>
+  );
+
+  const menuItems = getAllByRole('menuitem');
+  fireEvent.keyDown(menuItems[1], { key: 'ArrowRight', keyCode: right });
+  await waitFor(() => {
+    expect(menuItems[2]).toHaveFocus();
+    expect(document.activeElement).toBe(menuItems[2]);
+  });
+
+  fireEvent.keyDown(menuItems[0], { key: 'ArrowRight', keyCode: right });
+  await waitFor(() => {
+    expect(menuItems[1]).toHaveFocus();
+    expect(document.activeElement).toBe(menuItems[1]);
+  });
+});
+
+test('should pass-through classname', () => {
+  const { container } = render(
+    <MenuBar className="test">
+      <TopBarItem>1</TopBarItem>
+    </MenuBar>
+  );
+
+  // Assert that the container has the passed className
+  expect(container.firstChild).toHaveClass('test');
+});
+
+test('should return no axe violations', async () => {
+  const { container } = render(
+    <MenuBar>
+      <TopBarItem>1</TopBarItem>
+      <TopBarItem>2</TopBarItem>
+      <TopBarItem>3</TopBarItem>
+    </MenuBar>
+  );
+
+  // Perform accessibility test
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+});
