737cf77a872f7cb97b1e75df96e136011c9054da
Jason <jason@scurker.com>
2024-06-26 13:35:46 -0500

    cleanup tests and cover remaining use cases

--- packages/react/src/components/OptionsMenu/OptionsMenu.test.tsx
+++ packages/react/src/components/OptionsMenu/OptionsMenu.test.tsx
@@ -3,11 +3,23 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import OptionsMenu from './';
 import axe from '../../axe';
 
-const trigger = (props: any) => (
+const trigger = (props: React.HTMLAttributes<HTMLButtonElement>) => (
   <button type="button" {...props}>
-    foo
+    trigger
   </button>
 );
+
+test('should render menu', () => {
+  render(
+    <OptionsMenu trigger={trigger}>
+      <li>option 1</li>
+      <li>option 2</li>
+    </OptionsMenu>
+  );
+
+  expect(screen.queryByRole('menu')).toBeInTheDocument();
+  expect(screen.queryByRole('menu')).toHaveClass('OptionsMenu__list');
+});
 
 test('should render children', () => {
   render(
@@ -17,7 +29,7 @@ test('should render children', () => {
     </OptionsMenu>
   );
 
-  expect(screen.getAllByRole('menuitem')).toHaveLength(2);
+  expect(screen.queryAllByRole('menuitem')).toHaveLength(2);
 });
 
 test('should render trigger prop', () => {
@@ -27,18 +39,55 @@ test('should render trigger prop', () => {
     </OptionsMenu>
   );
 
-  expect(screen.getByRole('button')).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'trigger' })).toBeInTheDocument();
 });
 
-test('should pass className through', () => {
+test('should support className prop', () => {
   render(
-    <OptionsMenu trigger={trigger} className="foo bar">
+    <OptionsMenu trigger={trigger} className="bananas">
       <li>option 1</li>
       <li>option 2</li>
     </OptionsMenu>
   );
 
-  expect(screen.getByRole('button').parentElement).toHaveClass('foo bar');
+  expect(screen.queryByRole('menu')?.parentElement).toHaveClass(
+    'OptionsMenu bananas'
+  );
+});
+
+test('should support align prop', () => {
+  render(
+    <>
+      <OptionsMenu trigger={trigger} align="left">
+        <li>option 1</li>
+        <li>option 2</li>
+      </OptionsMenu>
+      <OptionsMenu trigger={trigger} align="right">
+        <li>option 1</li>
+        <li>option 2</li>
+      </OptionsMenu>
+    </>
+  );
+
+  expect(screen.queryAllByRole('menu')[0]?.parentElement).toHaveClass(
+    'OptionsMenu--align-left'
+  );
+  expect(screen.queryAllByRole('menu')[1]?.parentElement).toHaveClass(
+    'OptionsMenu--align-right'
+  );
+});
+
+test('should support menuRef prop', () => {
+  const menuRef = React.createRef<HTMLUListElement>();
+  render(
+    <OptionsMenu trigger={trigger} menuRef={menuRef}>
+      <li>option 1</li>
+      <li>option 2</li>
+    </OptionsMenu>
+  );
+
+  expect(menuRef.current).toBeTruthy();
+  expect(menuRef.current).toEqual(screen.getByRole('menu'));
 });
 
 test('should toggle menu on trigger clicks', () => {
@@ -98,9 +147,34 @@ test('should call onClose when closed', () => {
   expect(onClose).toBeCalled();
 });
 
-test('should return no axe violations', async () => {
+test('should close menu when click outside event occurs', () => {
+  render(
+    <>
+      <button>Click me!</button>
+      <OptionsMenu trigger={trigger}>
+        <li className="foo">option 1</li>
+      </OptionsMenu>
+    </>
+  );
+  fireEvent.click(screen.getByRole('button', { name: 'trigger' }));
+  expect(screen.getByRole('menu')).toHaveAttribute('aria-expanded', 'true');
+  fireEvent.click(screen.getByRole('button', { name: 'Click me!' }));
+  expect(screen.getByRole('menu')).toHaveAttribute('aria-expanded', 'false');
+});
+
+test('should return no axe violations when hidden', async () => {
   const { container } = render(
     <OptionsMenu trigger={trigger}>
+      <li className="foo">option 1</li>
+    </OptionsMenu>
+  );
+
+  expect(await axe(container)).toHaveNoViolations();
+});
+
+test('should return no axe violations when shown', async () => {
+  const { container } = render(
+    <OptionsMenu trigger={trigger} show={true}>
       <li className="foo">option 1</li>
     </OptionsMenu>
   );
