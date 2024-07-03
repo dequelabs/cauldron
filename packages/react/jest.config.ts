edfc22558063e6a4a26ccd9229ed9d0ddc3f2e9e
Anastasia Lanz <anastasia.lanz@deque.com>
2024-07-03 08:19:32 -0500

    chore: remove enzyme dependency

--- packages/react/jest.config.ts
+++ packages/react/jest.config.ts
@@ -4,8 +4,6 @@ const config: Config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testMatch: [
-    '**/__tests__/src/**/*.js',
-    '**/__tests__/demo/**/*.js',
     '**/src/components/**/*.test.tsx',
     '**/src/utils/**/*.test.{ts,tsx}',
     '**/src/contexts/**/*.test.tsx',
@@ -16,15 +14,7 @@ const config: Config = {
     '<rootDir>/src/utils/**/*.{tsx,ts}',
     '<rootDir>/src/contexts/**/*.{tsx,ts}',
     '!<rootDir>/src/utils/polymorphicComponent.test.tsx'
-  ],
-  moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/__tests__/styleMock.js',
-    'react-syntax-highlighter/dist/esm/light':
-      '<rootDir>/__tests__/reactSyntaxHighlighterMock.js',
-    'react-syntax-highlighter/dist/esm/languages/hljs/(.*)':
-      '<rootDir>/__tests__/hljsMock.js',
-    '\\.svg$': '<rootDir>/__tests__/svgMock.js'
-  }
+  ]
 };
 
 export default config;
