fa32608fa52a8b07e90448dc5dd96c8213478002
Jason <jason@scurker.com>
2024-09-20 17:12:59 -0500

    fix file

--- packages/react/src/utils/resolveElement.test.ts
+++ packages/react/src/utils/resolveElement.test.ts
@@ -1,33 +1,33 @@
 import { createRef, type MutableRefObject } from 'react';
-import getElementOrRef from './getElementOrRef';
+import resolveElement from './resolveElement';
 
 test('should return element', () => {
-  expect(getElementOrRef(document.body)).toBe(document.body);
+  expect(resolveElement(document.body)).toBe(document.body);
 });
 
 test('should return ref element', () => {
   const ref = createRef() as MutableRefObject<HTMLElement>;
   ref.current = document.body;
-  expect(getElementOrRef(ref)).toBe(document.body);
+  expect(resolveElement(ref)).toBe(document.body);
 });
 
 test('should return null when element is undefined', () => {
-  expect(getElementOrRef(undefined)).toBe(null);
+  expect(resolveElement(undefined)).toBe(null);
 });
 
 test('should return null when ref is undefined', () => {
   const ref = createRef() as MutableRefObject<HTMLElement>;
-  expect(getElementOrRef(ref)).toBe(null);
+  expect(resolveElement(ref)).toBe(null);
 });
 
 test('should return null when element is not instance of Element', () => {
   // @ts-expect-error bad data
-  expect(getElementOrRef('thing')).toBe(null);
+  expect(resolveElement('thing')).toBe(null);
 });
 
 test('should return null when ref is not instance of Element', () => {
   const ref = createRef() as MutableRefObject<HTMLElement>;
   // @ts-expect-error bad data
   ref.current = 'thing';
-  expect(getElementOrRef(ref)).toBe(null);
+  expect(resolveElement(ref)).toBe(null);
 });
