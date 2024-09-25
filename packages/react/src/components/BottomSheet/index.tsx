b3a7b1a5d35393a4018f290f0a1a833152b133c9
Jason <jason@scurker.com>
2024-09-25 09:38:34 -0500

    feat(react,styles): add BottomSheet component

---
+++ packages/react/src/components/BottomSheet/index.tsx
@@ -0,0 +1,80 @@
+import React, { forwardRef, useRef, useCallback } from 'react';
+import { useId } from 'react-id-generator';
+import classnames from 'classnames';
+import Drawer from '../Drawer';
+import IconButton from '../IconButton';
+import { ContentNode } from '../../types';
+import useSharedRef from '../../utils/useSharedRef';
+
+type BottomSheetProps = {
+  label: ContentNode;
+  closeButtonText?: ContentNode;
+  open?: boolean;
+} & React.HTMLAttributes<HTMLDivElement> &
+  Pick<React.ComponentProps<typeof Drawer>, 'open' | 'onClose'>;
+
+const BottomSheet = forwardRef<HTMLDivElement, BottomSheetProps>(
+  (
+    {
+      label,
+      children,
+      className,
+      closeButtonText = 'Close',
+      open = false,
+      onClose,
+      ...props
+    },
+    ref
+  ) => {
+    const bottomSheetRef = useSharedRef(ref);
+    const [labelId] = useId(1, 'bottom-sheet-label');
+
+    const handleClose = useCallback(() => {
+      if (typeof onClose === 'function') {
+        onClose();
+      }
+    }, [onClose]);
+
+    return (
+      <Drawer
+        position="bottom"
+        open={open}
+        focusTrap
+        focusOptions={{
+          initialFocus: bottomSheetRef
+        }}
+        onClose={handleClose}
+        {...props}
+      >
+        <div
+          ref={bottomSheetRef}
+          className={classnames('BottomSheet', className)}
+          tabIndex={-1}
+          role="dialog"
+          aria-labelledby={labelId}
+        >
+          <div className="BottomSheet__header">
+            <div id={labelId} className="BottomSheet__title">
+              {label}
+            </div>
+            <IconButton
+              icon="close"
+              label={closeButtonText}
+              aria-label={`${closeButtonText}`}
+              onClick={handleClose}
+              tooltipProps={{
+                show: false,
+                association: 'none'
+              }}
+            />
+          </div>
+          <div className="BottomSheet__contents">{children}</div>
+        </div>
+      </Drawer>
+    );
+  }
+);
+
+BottomSheet.displayName = 'BottomSheet';
+
+export default BottomSheet;
