983c89f21b584f1cef2c405c9e2db03529edbec1
Jason <jason@scurker.com>
2024-11-22 10:04:47 -0600

    feat(react,styles): add grid layout for table component (#1748)

--- packages/react/src/components/Table/TableCell.tsx
+++ packages/react/src/components/Table/TableCell.tsx
@@ -1,14 +1,37 @@
-import React, { TdHTMLAttributes } from 'react';
+import type { ColumnAlignment } from './Table';
+import React, { forwardRef } from 'react';
 import classNames from 'classnames';
+import { useTable } from './TableContext';
+import useTableGridStyles from './useTableGridStyles';
+import useSharedRef from '../../utils/useSharedRef';
 
-const TableCell = ({
-  children,
-  className,
-  ...other
-}: TdHTMLAttributes<HTMLTableCellElement>) => (
-  <td className={classNames('TableCell', className)} {...other}>
-    {children}
-  </td>
+interface TableCellProps
+  extends Omit<React.TdHTMLAttributes<HTMLTableDataCellElement>, 'align'> {
+  align?: ColumnAlignment;
+}
+
+const TableCell = forwardRef<HTMLTableCellElement, TableCellProps>(
+  ({ children, className, align, style, ...other }, ref) => {
+    const tableCellRef = useSharedRef<HTMLTableCellElement>(ref);
+    const { layout, columns } = useTable();
+    const tableGridStyles = useTableGridStyles({
+      elementRef: tableCellRef,
+      align,
+      columns,
+      layout
+    });
+
+    return (
+      <td
+        ref={tableCellRef}
+        style={{ ...tableGridStyles, ...style }}
+        className={classNames('TableCell', className)}
+        {...other}
+      >
+        {children}
+      </td>
+    );
+  }
 );
 
 TableCell.displayName = 'TableCell';
