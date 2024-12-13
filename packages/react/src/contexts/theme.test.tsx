4505ef2e01a31aceeaa4a5e728e6b418645158c4
Jason <jason@scurker.com>
2024-12-13 15:51:06 -0600

    test: fix act errors in react tests (#1761)

--- packages/react/src/contexts/theme.test.tsx
+++ packages/react/src/contexts/theme.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import { ThemeProvider, useThemeContext } from './theme';
-import { render } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 
 let theme: string,
   toggleTheme: () => void,
