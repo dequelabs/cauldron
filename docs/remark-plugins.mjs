136cb9487ab3902214dd59f13fd000539ab32046
Stephen Mathieson <mathiesonstephen8@gmail.com>
2025-02-06 17:17:14 -0500

    style: Run Prettier
    Diff generated via `npx prettier --write .`

--- docs/remark-plugins.mjs
+++ docs/remark-plugins.mjs
@@ -1,16 +1,16 @@
 /* Note: This file imports esm only packages, so it _has_ to be mjs */
-import remarkFrontmatter from 'remark-frontmatter'
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
-import { remarkMdxToc } from 'remark-mdx-toc'
-import remarkGfm from 'remark-gfm'
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import { remarkMdxToc } from 'remark-mdx-toc';
+import remarkGfm from 'remark-gfm';
 
 import { visit } from 'unist-util-visit';
 import { fromMarkdown } from 'mdast-util-from-markdown';
 import { mdxjs } from 'micromark-extension-mdxjs';
 import { mdxFromMarkdown } from 'mdast-util-mdx';
-import { mdxExpressionFromMarkdown } from 'mdast-util-mdx-expression'
+import { mdxExpressionFromMarkdown } from 'mdast-util-mdx-expression';
 
-const index = 0
+const index = 0;
 
 const exampleFromMDX = () => {
   return (tree) => {
@@ -22,22 +22,19 @@ const exampleFromMDX = () => {
 
       const options = {
         extensions: [mdxjs()],
-        mdastExtensions: [
-          mdxFromMarkdown(),
-          mdxExpressionFromMarkdown
-        ]
+        mdastExtensions: [mdxFromMarkdown(), mdxExpressionFromMarkdown]
       };
 
       // HACKY - using an AST parser to detect functions
       // would be a better (and safer) experience than this
-      let func = ''
-      let render = ''
+      let func = '';
+      let render = '';
       if (raw.startsWith('function')) {
         // Try to parse the name
-        const [,name] = raw.match(/^function\s+(\w+)/)
+        const [, name] = raw.match(/^function\s+(\w+)/);
         if (name) {
-          func = `\n\nexport ${raw}\n\n`
-          render = `<${name} />`
+          func = `\n\nexport ${raw}\n\n`;
+          render = `<${name} />`;
         }
       }
 
@@ -45,27 +42,24 @@ const exampleFromMDX = () => {
         // We want to ensure that any react markup renders pure and does
         // not get rendered using markdown components, so export the code
         // wrapped in a function to ensure components do not get applied
-        index++
-        func = `\n\nexport function CodeExample${index}() { return <>${raw}</> }\n\n`
-        render = `<CodeExample${index} />`
+        index++;
+        func = `\n\nexport function CodeExample${index}() { return <>${raw}</> }\n\n`;
+        render = `<CodeExample${index} />`;
       }
 
-      const exampleMarkdown =  `${func}<Example raw={''}>
+      const exampleMarkdown = `${func}<Example raw={''}>
 {
   <div className="Component__example__react">
   ${render || raw}
   </div>
 }
-</Example>`
+</Example>`;
 
-      const updatedNode = fromMarkdown(
-        exampleMarkdown,
-        options
-      );
+      const updatedNode = fromMarkdown(exampleMarkdown, options);
 
       // Note: From markdown will strip extra whitespace from raw code, which
       // we want to preserve so manually include it in the mdast
-      updatedNode.children[func ? 1 : 0].attributes[0].value = raw
+      updatedNode.children[func ? 1 : 0].attributes[0].value = raw;
 
       Object.assign(node, updatedNode);
     });
@@ -78,4 +72,4 @@ export default [
   remarkGfm,
   remarkMdxToc,
   exampleFromMDX
-]
+];
