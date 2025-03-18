3f552020bd21547049dd6ea7fc37dd350946378a
Jason <jason@scurker.com>
2025-03-18 16:42:59 -0500

    no timeout?

--- e2e/accessibility.ts
+++ e2e/accessibility.ts
@@ -69,7 +69,7 @@ const main = async (): Promise<void> => {
         const component = (url.split('/').pop() as string) || 'Index';
 
         try {
-          await page.goto(url, { waitUntil: 'networkidle0' });
+          await page.goto(url, { waitUntil: 'networkidle0', timeout: 0 });
         } catch (ex) {
           console.log(url);
           console.log(ex);
