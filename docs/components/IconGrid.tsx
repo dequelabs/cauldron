e71ae1672bd1f6f03f3ade857f42b69a4b784f4a
Jason <jason@scurker.com>
2025-03-20 15:13:51 -0500

    docs: fix announcement for icon search (#1793)

--- docs/components/IconGrid.tsx
+++ docs/components/IconGrid.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import {
   Icon,
   iconTypes,
+  IconType,
   TextField,
   Panel,
   Button
@@ -11,6 +12,7 @@ import './IconGrid.css';
 export default function IconGrid() {
   const inputRef = useRef<HTMLInputElement>(null);
   const [searchValue, setSearchValue] = useState('');
+  const [showDelayedAnnouncement, setShowDelayedAnnouncement] = useState(false);
   const filteredTypes = searchValue.trim().length
     ? iconTypes.filter((type: string) =>
         type.includes(searchValue.trim().toLowerCase())
@@ -26,6 +28,17 @@ export default function IconGrid() {
     inputRef.current?.focus();
   };
 
+  const noMatchesFound = !filteredTypes.length;
+
+  useEffect(() => {
+    if (noMatchesFound) {
+      // according to the aria spec, status is a live region and only dynamic
+      // changes to content should be announced. We're artificially delaying the
+      // status content in order to ensure this gets announced for AT users
+      setTimeout(() => setShowDelayedAnnouncement(true), 100);
+    }
+  }, [noMatchesFound]);
+
   return (
     <>
       <TextField
@@ -37,24 +50,28 @@ export default function IconGrid() {
         onChange={handleChange}
       />
       <div className="IconGrid">
-        {filteredTypes.length ? (
-          <ul>
-            {filteredTypes.map((type: string) => (
-              <li className="Panel" key={type}>
-                <Icon type={type} />
-                <div className="IconGrid__label">{type}</div>
-              </li>
-            ))}
-          </ul>
-        ) : (
+        {noMatchesFound ? (
           <Panel className="IconGrid__NoResults">
             <p role="status">
-              No icon types found matching <strong>"{searchValue}"</strong>.
+              {showDelayedAnnouncement ? (
+                <>
+                  No icon types found matching <strong>"{searchValue}"</strong>.
+                </>
+              ) : null}
             </p>
             <Button variant="secondary" onClick={handleClick}>
               Clear Search
             </Button>
           </Panel>
+        ) : (
+          <ul>
+            {filteredTypes.map((type: string) => (
+              <li className="Panel" key={type}>
+                <Icon type={type as IconType} />
+                <div className="IconGrid__label">{type}</div>
+              </li>
+            ))}
+          </ul>
         )}
       </div>
     </>
