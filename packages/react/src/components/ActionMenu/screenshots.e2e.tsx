2ae727b2519408d2d9ae488a1fd69b7adceb7ca3
Jason <jason@scurker.com>
2025-04-23 19:16:41 -0500

    more screenshot states

--- packages/react/src/components/ActionMenu/screenshots.e2e.tsx
+++ packages/react/src/components/ActionMenu/screenshots.e2e.tsx
@@ -40,6 +40,8 @@ test('should have screenshot for ActionMenu', async ({ mount, page }) => {
           <ActionListItem disabled>Disabled</ActionListItem>
           <ActionListItem>Hover</ActionListItem>
           <ActionListLinkItem href="#">Link</ActionListLinkItem>
+          <ActionListItem leadingIcon="copy">Leading Icon</ActionListItem>
+          <ActionListItem trailingIcon="new">Trailing Icon</ActionListItem>
           <ActionListSeparator />
           <ActionListItem>Separated</ActionListItem>
         </ActionList>
@@ -77,11 +79,63 @@ test('should have screenshot for ActionMenu selections', async ({
           </Button>
         }
       >
-        <ActionList>
+        <ActionList selectionType="single">
           <ActionListGroup label="Group Label">
             <ActionListItem>Focus</ActionListItem>
             <ActionListItem>Group Item 2</ActionListItem>
             <ActionListItem>Group Item 3</ActionListItem>
+          </ActionListGroup>
+          <ActionListItem>Item</ActionListItem>
+          <ActionListItem selected>Selected</ActionListItem>
+          <ActionListItem disabled>Disabled</ActionListItem>
+          <ActionListItem selected disabled>
+            Selected Disabled
+          </ActionListItem>
+          <ActionListItem>Hover</ActionListItem>
+          <ActionListLinkItem href="#">Link</ActionListLinkItem>
+          <ActionListItem leadingIcon="copy">Leading Icon</ActionListItem>
+          <ActionListItem trailingIcon="new">Trailing Icon</ActionListItem>
+          <ActionListSeparator />
+          <ActionListItem>Separated</ActionListItem>
+        </ActionList>
+      </ActionMenu>
+    </div>
+  );
+
+  await component.getByText('Action Menu Trigger').click();
+  await component.getByText('Hover').hover();
+  // await component.getByText('Focus').focus();
+
+  await expect(component).toHaveScreenshot('actionmenu-selections');
+  await setTheme(page, 'dark');
+  await expect(component).toHaveScreenshot('dark-actionmenu-selections');
+});
+
+test('should have screenshot for ActionMenu mixed', async ({ mount, page }) => {
+  await page.addStyleTag({
+    // ensure screenshots are able to capture the boundaries of an expanded combobox
+    content: `.ActionMenu {
+      position: relative !important;
+      transform: translateY(2px) !important;
+    }`
+  });
+  const component = await mount(
+    <div>
+      <ActionMenu
+        trigger={
+          <Button variant="secondary">
+            Action Menu Trigger
+            <Icon type="chevron-down" />
+          </Button>
+        }
+      >
+        <ActionList>
+          <ActionListGroup label="Group Label" selectionType="single">
+            <ActionListItem>Focus</ActionListItem>
+            <ActionListItem selected>Selected</ActionListItem>
+            <ActionListItem selected disabled>
+              Selected Disabled
+            </ActionListItem>
           </ActionListGroup>
           <ActionListItem>Item</ActionListItem>
           <ActionListItem disabled>Disabled</ActionListItem>
@@ -100,7 +154,7 @@ test('should have screenshot for ActionMenu selections', async ({
   await component.getByText('Hover').hover();
   // await component.getByText('Focus').focus();
 
-  await expect(component).toHaveScreenshot('actionmenu');
+  await expect(component).toHaveScreenshot('actionmenu-mixed');
   await setTheme(page, 'dark');
-  await expect(component).toHaveScreenshot('dark-actionmenu');
+  await expect(component).toHaveScreenshot('dark-actionmenu-mixed');
 });
