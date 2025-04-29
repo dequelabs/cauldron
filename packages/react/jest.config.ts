efbbd5fa9ae752af5ab70b9d09b077062198a51b
Jason <jason@scurker.com>
2025-04-29 09:26:48 -0500

    chore: fix code coverage (#1889)

--- packages/react/jest.config.ts
+++ packages/react/jest.config.ts
@@ -13,8 +13,17 @@ const config: Config = {
     '<rootDir>/src/components/**/*.{tsx,ts}',
     '<rootDir>/src/utils/**/*.{tsx,ts}',
     '<rootDir>/src/contexts/**/*.{tsx,ts}',
-    '!<rootDir>/src/utils/polymorphicComponent.test.tsx'
-  ]
+    '!<rootDir>/src/utils/polymorphicComponent.test.tsx',
+    '!<rootDir>/**/*.e2e.tsx'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      statements: 90,
+      lines: 90,
+      functions: 85
+    }
+  }
 };
 
 export default config;
