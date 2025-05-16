64502018b52fd43aaa0093c5f05ebc95f192511e
Jason <jason@scurker.com>
2025-05-16 11:55:31 -0500

    Revert "feat(style): update panel radius and padding (#1905)" (#1936)
    This reverts commit b541dd62baa298e381e45d9924b79084099c751b.

--- packages/react/src/components/Combobox/screenshots.e2e.tsx
+++ packages/react/src/components/Combobox/screenshots.e2e.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { test, expect } from '../../../../../e2e/screenshots';
-import { setTheme } from '../../../../../e2e/helpers/playwright';
-import { Combobox, ComboboxOption } from '../../../';
+import { setActive, setTheme } from '../../../../../e2e/helpers/playwright';
+import { Combobox, ComboboxOption, FieldWrap } from '../../../';
 
 test('should have screenshot for Combobox', async ({ mount, page }) => {
   await page.addStyleTag({
     // ensure screenshots are able to capture the boundaries of an expanded combobox
-    content: `
-      .Combobox__listbox--open.Combobox__listbox { position: relative !important; }
-      body.cauldron--theme-dark { background-color: var(--accent-medium); }
-    `
+    content: `.Combobox__listbox--open.Combobox__listbox { position: relative !important; }`
   });
   const component = await mount(
-    <div>
+    <FieldWrap>
       <Combobox label="Combobox">
         <ComboboxOption>Apple</ComboboxOption>
         <ComboboxOption>Banana</ComboboxOption>
@@ -28,7 +25,7 @@ test('should have screenshot for Combobox', async ({ mount, page }) => {
         <ComboboxOption>Banana</ComboboxOption>
         <ComboboxOption>Cucumber</ComboboxOption>
       </Combobox>
-    </div>
+    </FieldWrap>
   );
 
   await component.getByRole('combobox', { name: 'Focus' }).focus();
@@ -45,13 +42,10 @@ test('should have screenshot for Combobox[multiselect]', async ({
 }) => {
   await page.addStyleTag({
     // ensure screenshots are able to capture the boundaries of an expanded combobox
-    content: `
-      .Combobox__listbox--open.Combobox__listbox { position: relative !important; }
-      body.cauldron--theme-dark { background-color: var(--accent-medium); }
-    `
+    content: `.Combobox__listbox--open.Combobox__listbox { position: relative !important; }`
   });
   const component = await mount(
-    <div>
+    <FieldWrap>
       <Combobox label="Combobox" multiselect>
         <ComboboxOption>Apple</ComboboxOption>
         <ComboboxOption>Banana</ComboboxOption>
@@ -77,7 +71,7 @@ test('should have screenshot for Combobox[multiselect]', async ({
         <ComboboxOption>Banana</ComboboxOption>
         <ComboboxOption>Cucumber</ComboboxOption>
       </Combobox>
-    </div>
+    </FieldWrap>
   );
 
   await component.getByRole('combobox', { name: 'Focus' }).focus();
@@ -91,13 +85,10 @@ test('should have screenshot for Combobox[multiselect]', async ({
 test('should have screenshot for Combobox[error]', async ({ mount, page }) => {
   await page.addStyleTag({
     // ensure screenshots are able to capture the boundaries of an expanded combobox
-    content: `
-      .Combobox__listbox--open.Combobox__listbox { position: relative !important; }
-      body.cauldron--theme-dark { background-color: var(--accent-medium); }
-    `
+    content: `.Combobox__listbox--open.Combobox__listbox { position: relative !important; }`
   });
   const component = await mount(
-    <div>
+    <FieldWrap>
       <Combobox label="Combobox" error="This field has an error.">
         <ComboboxOption>Apple</ComboboxOption>
         <ComboboxOption>Banana</ComboboxOption>
@@ -113,7 +104,7 @@ test('should have screenshot for Combobox[error]', async ({ mount, page }) => {
         <ComboboxOption>Banana</ComboboxOption>
         <ComboboxOption>Cucumber</ComboboxOption>
       </Combobox>
-    </div>
+    </FieldWrap>
   );
 
   await component.getByRole('combobox', { name: 'Focus' }).focus();
