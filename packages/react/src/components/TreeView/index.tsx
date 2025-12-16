e3b8e65f7d4f5324ff3b91c6fcd8baddeffc0203
JoshMK <joshuakirwin@gmail.com>
2025-12-16 10:27:54 -0600

    feat: add props

--- packages/react/src/components/TreeView/index.tsx
+++ packages/react/src/components/TreeView/index.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
-import { Tree } from 'react-aria-components';
-import TreeViewTreeItem from './TreeViewTreeItem';
+import { Collection, Tree, TreeItem } from 'react-aria-components';
+import TreeViewTreeItemContent from './TreeViewTreeItemContent';
 
 interface TreeViewProps {
-  children: React.ReactNode;
+  selectionBehavior?: 'replace' | 'toggle';
+  selectionMode?: 'none' | 'single' | 'multiple';
+  defaultExpandedKeys?: string[];
+  items: TreeViewFileType[];
+}
+
+interface TreeViewFileType {
+  id: string;
+  title: string;
+  type: 'directory' | 'file';
+  children?: TreeViewFileType[];
 }
 
 export const TreeView = (props: TreeViewProps) => {
   return (
-    <Tree
-      aria-label="Files"
-      selectionBehavior="toggle"
-      selectionMode="multiple"
-      defaultExpandedKeys={['documents', 'photos', 'project']}
-    >
-      <TreeViewTreeItem id="documents" title="Documents">
-        <TreeViewTreeItem id="project" title="Project">
-          <TreeViewTreeItem id="report" title="Weekly Report" />
-        </TreeViewTreeItem>
-      </TreeViewTreeItem>
-      <TreeViewTreeItem id="photos" title="Photos">
-        <TreeViewTreeItem id="one" title="Image 1" />
-        <TreeViewTreeItem id="two" title="Image 2" />
-      </TreeViewTreeItem>
+    <Tree {...props}>
+      {function renderItem({ title, children }: TreeViewFileType) {
+        return (
+          <TreeItem textValue={title}>
+            <TreeViewTreeItemContent>{title}</TreeViewTreeItemContent>
+            <Collection items={children}>
+              {/* recursively render children */}
+              {children && renderItem}
+            </Collection>
+          </TreeItem>
+        );
+      }}
     </Tree>
   );
 };
