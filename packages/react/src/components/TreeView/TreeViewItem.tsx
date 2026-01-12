066a95b75d291fff9716b9353131a3712908eae0
JoshMK <joshuakirwin@gmail.com>
2026-01-12 12:55:29 -0600

    fix: update docs, bake onaction fix into component

--- packages/react/src/components/TreeView/TreeViewItem.tsx
+++ packages/react/src/components/TreeView/TreeViewItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   Button,
   Collection,
@@ -19,31 +19,56 @@ const TreeViewItem = ({
   textValue,
   children,
   onAction
-}: TreeViewItemProps) => (
-  <TreeItem key={id} id={id} textValue={textValue} onAction={onAction}>
-    <TreeItemContent>
-      {({ selectionMode, isSelected }: TreeItemContentRenderProps) => (
-        <>
-          <Button slot="chevron">
-            <Icon type="chevron-right" />
-          </Button>
-          {selectionMode !== 'none' ? (
-            <Checkbox id={id} label={textValue} checked={isSelected} />
-          ) : (
-            <>{textValue}</>
-          )}
-        </>
+}: TreeViewItemProps) => {
+  const treeItemRef = useRef(null);
+
+  //by default, react-aria-components doesn't toggle the selected state if you provide a custom onAction handler.
+  const handleOnAction = (key: string, onAction?: (key: string) => void) => {
+    const elm = treeItemRef.current;
+    if (elm) {
+      const selected = (elm as HTMLDivElement).getAttribute('aria-selected');
+      (elm as HTMLDivElement).setAttribute(
+        'aria-selected',
+        selected === 'true' ? 'false' : 'true'
+      );
+      if (onAction) {
+        onAction(id);
+      }
+    }
+  };
+
+  return (
+    <TreeItem
+      key={id}
+      id={id}
+      textValue={textValue}
+      onAction={() => handleOnAction(id, onAction)}
+      ref={treeItemRef}
+    >
+      <TreeItemContent>
+        {({ selectionMode, isSelected }: TreeItemContentRenderProps) => (
+          <>
+            <Button slot="chevron">
+              <Icon type="chevron-right" />
+            </Button>
+            {selectionMode !== 'none' ? (
+              <Checkbox id={id} label={textValue} checked={isSelected} />
+            ) : (
+              <>{textValue}</>
+            )}
+          </>
+        )}
+      </TreeItemContent>
+      {children && children?.length > 0 && (
+        <Collection items={children}>
+          {children.map((child) => (
+            <TreeViewItem key={child.id} {...child} />
+          ))}
+        </Collection>
       )}
-    </TreeItemContent>
-    {children && children?.length > 0 && (
-      <Collection items={children}>
-        {children.map((child) => (
-          <TreeViewItem key={child.id} {...child} />
-        ))}
-      </Collection>
-    )}
-  </TreeItem>
-);
+    </TreeItem>
+  );
+};
 
 TreeViewItem.displayName = 'TreeViewItem';
 
