32c194698bbeae61d8658d1fff2c7c2106001990
Thomas Braccia <bracciata@gmail.com>
2026-04-21 16:03:41 -0400

    fix: increase contrast and correct screenshot names

--- packages/react/src/components/TextEllipsis/screenshots.e2e.tsx
+++ packages/react/src/components/TextEllipsis/screenshots.e2e.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { test, expect } from '../../../../../e2e/screenshots';
 import { setTheme } from '../../../../../e2e/helpers/playwright';
-import { TextEllipsis } from '../../../';
+import { Panel, TextEllipsis } from '../../../';
 
 const overflowText =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 
 test('should have screenshot for TextEllipsis', async ({ mount, page }) => {
   const component = await mount(
-    <div style={{ width: '200px' }}>
+    <Panel style={{ width: '200px' }}>
       <TextEllipsis>{overflowText}</TextEllipsis>
-    </div>
+    </Panel>
   );
 
   await expect(component).toHaveScreenshot('text-ellipsis');
@@ -23,9 +23,9 @@ test('should have screenshot for TextEllipsis tooltip on hover', async ({
   page
 }) => {
   await mount(
-    <div style={{ width: '200px' }}>
+    <Panel style={{ width: '200px' }}>
       <TextEllipsis>{overflowText}</TextEllipsis>
-    </div>
+    </Panel>
   );
 
   const ellipsis = page.getByRole('button');
@@ -43,14 +43,14 @@ test('should have screenshot for TextEllipsis[maxLines=2]', async ({
   page
 }) => {
   const component = await mount(
-    <div style={{ width: '200px' }}>
+    <Panel style={{ width: '200px' }}>
       <TextEllipsis maxLines={2}>{overflowText}</TextEllipsis>
-    </div>
+    </Panel>
   );
 
-  await expect(component).toHaveScreenshot('text-ellipsis[maxLines=2]');
+  await expect(component).toHaveScreenshot('text-ellipsis-max-lines-2');
   await setTheme(page, 'dark');
-  await expect(component).toHaveScreenshot('dark--text-ellipsis[maxLines=2]');
+  await expect(component).toHaveScreenshot('dark--text-ellipsis-max-lines-2');
 });
 
 test('should have screenshot for TextEllipsis[hideTooltip]', async ({
@@ -58,16 +58,16 @@ test('should have screenshot for TextEllipsis[hideTooltip]', async ({
   page
 }) => {
   await mount(
-    <div style={{ width: '200px' }}>
+    <Panel style={{ width: '200px' }}>
       <TextEllipsis hideTooltip>{overflowText}</TextEllipsis>
-    </div>
+    </Panel>
   );
 
   const ellipsis = page.locator('.TextEllipsis');
   await ellipsis.hover();
   await expect(page.locator('.Tooltip')).toHaveCount(0);
 
-  await expect(page).toHaveScreenshot('text-ellipsis[hideTooltip]');
+  await expect(page).toHaveScreenshot('text-ellipsis-hide-tooltip');
 });
 
 test('should have screenshot for TextEllipsis[hideTooltip][maxLines=2]', async ({
@@ -75,18 +75,18 @@ test('should have screenshot for TextEllipsis[hideTooltip][maxLines=2]', async (
   page
 }) => {
   await mount(
-    <div style={{ width: '200px' }}>
+    <Panel style={{ width: '200px' }}>
       <TextEllipsis hideTooltip maxLines={2}>
         {overflowText}
       </TextEllipsis>
-    </div>
+    </Panel>
   );
 
   const ellipsis = page.locator('.TextEllipsis');
   await ellipsis.hover();
   await expect(page.locator('.Tooltip')).toHaveCount(0);
 
-  await expect(page).toHaveScreenshot('text-ellipsis[hideTooltip][maxLines=2]');
+  await expect(page).toHaveScreenshot('text-ellipsis-hide-tooltip-max-lines-2');
 });
 
 test('should have screenshot for TextEllipsis without overflow', async ({
@@ -94,9 +94,9 @@ test('should have screenshot for TextEllipsis without overflow', async ({
   page
 }) => {
   const component = await mount(
-    <div style={{ width: '200px' }}>
+    <Panel style={{ width: '200px' }}>
       <TextEllipsis>Short text</TextEllipsis>
-    </div>
+    </Panel>
   );
 
   await expect(component).toHaveScreenshot('text-ellipsis-no-overflow');
