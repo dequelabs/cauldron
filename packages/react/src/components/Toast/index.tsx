77eea7acac85901af6f176a20e4af71d810bc8a7
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2026-04-23 12:01:32 +0300

    refactor(Toast): update toast to be functional component

--- packages/react/src/components/Toast/index.tsx
+++ packages/react/src/components/Toast/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import classNames from 'classnames';
 import Icon from '../Icon';
 import AriaIsolate from '../../utils/aria-isolate';
@@ -7,192 +7,166 @@ import setRef from '../../utils/setRef';
 
 export interface ToastProps extends React.HTMLAttributes<HTMLDivElement> {
   type: 'confirmation' | 'caution' | 'error' | 'action-needed' | 'info';
-  onDismiss: () => void;
+  onDismiss?: () => void;
   dismissText?: string;
-  toastRef: React.Ref<HTMLDivElement>;
+  toastRef?: React.Ref<HTMLDivElement>;
   focus?: boolean;
   show?: boolean;
   dismissible?: boolean;
   children: React.ReactNode;
 }
 
-interface ToastState {
-  animationClass: string;
-  isolator?: AriaIsolate;
-}
-
 /**
  * The cauldron toast notification component
  */
-export default class Toast extends React.Component<ToastProps, ToastState> {
-  static defaultProps = {
-    dismissText: 'Dismiss',
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    onDismiss: () => {},
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    toastRef: () => {},
-    focus: true,
-    show: false,
-    dismissible: true
+const Toast = ({
+  type,
+  children,
+  onDismiss,
+  dismissText = 'Dismiss',
+  toastRef,
+  focus = true,
+  show = false,
+  dismissible = true,
+  className,
+  ...otherProps
+}: ToastProps) => {
+  const [animationClass, setAnimationClass] = useState(
+    show ? 'FadeIn--flex' : 'is--hidden'
+  );
+
+  const elRef = useRef<HTMLDivElement | null>(null);
+  const isolatorRef = useRef<AriaIsolate | undefined>(undefined);
+  const isFirstRenderRef = useRef(true);
+
+  // Keep refs to latest prop values to avoid stale closures in effects/callbacks
+  const typeRef = useRef(type);
+  const focusRef = useRef(focus);
+  const onDismissRef = useRef(onDismiss);
+  typeRef.current = type;
+  focusRef.current = focus;
+  onDismissRef.current = onDismiss;
+
+  const showToast = () => {
+    const el = elRef.current;
+    setAnimationClass('FadeIn--flex FadeIn');
+
+    if (typeRef.current === 'action-needed') {
+      const newIsolator = new AriaIsolate(el as HTMLDivElement);
+      tabIndexHandler(false, el);
+      isolatorRef.current = newIsolator;
+      newIsolator.activate();
+    }
+
+    if (el && focusRef.current) {
+      el.focus();
+    }
   };
 
-  static displayName = 'Toast';
+  const dismissToast = () => {
+    const el = elRef.current;
+    if (!el) return;
 
-  private el: HTMLDivElement | null;
+    setAnimationClass('FadeIn--flex');
 
-  constructor(props: ToastProps) {
-    super(props);
+    // Timeout because CSS display: none/block and opacity:
+    // 0/1 properties cannot be toggled in the same tick
+    // see: https://codepen.io/isnerms/pen/eyQaLP
+    setTimeout(() => {
+      if (typeRef.current === 'action-needed') {
+        tabIndexHandler(true, el);
+        isolatorRef.current?.deactivate();
+      }
 
-    this.state = {
-      animationClass: props.show ? 'FadeIn--flex' : 'is--hidden'
-    };
+      setAnimationClass('is--hidden');
+      onDismissRef.current?.();
+    });
+  };
 
-    this.dismissToast = this.dismissToast.bind(this);
-    this.showToast = this.showToast.bind(this);
-  }
-
-  componentDidMount() {
-    const { show } = this.props;
-
+  // Mount effect: if initially shown, trigger the show animation
+  useEffect(() => {
     if (show) {
       // Timeout because CSS display: none/block and opacity:
       // 0/1 properties cannot be toggled in the same tick
       // see: https://codepen.io/isnerms/pen/eyQaLP
-      setTimeout(this.showToast);
+      const id = setTimeout(showToast);
+      return () => clearTimeout(id);
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps: ToastProps) {
-    const { show } = this.props;
-    if (prevProps.show !== show) {
-      if (show) {
-        this.setState({ animationClass: 'FadeIn--flex' }, () => {
-          setTimeout(this.showToast);
-        });
-      } else {
-        this.dismissToast();
-      }
-    }
-  }
-
-  componentWillUnmount() {
-    const { isolator } = this.state;
-    isolator?.deactivate();
-  }
-
-  render() {
-    const { animationClass } = this.state;
-    const {
-      type,
-      children,
-      // prevent `onDismiss` from being passed-through to DOM
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      onDismiss,
-      dismissText,
-      toastRef,
-      focus,
-      show,
-      dismissible,
-      className,
-      ...otherProps
-    } = this.props;
-    const scrim =
-      type === 'action-needed' && show ? (
-        <div className="Scrim--light Scrim--show Scrim--fade-in" />
-      ) : null;
-
-    const defaultProps: React.HTMLAttributes<HTMLDivElement> = {
-      tabIndex: -1,
-      className: classNames(
-        'Toast',
-        `Toast--${typeMap[type].className}`,
-        animationClass,
-        { 'Toast--non-dismissible': !dismissible },
-        className
-      )
-    };
-
-    if (!focus) {
-      defaultProps.role = 'alert';
-    }
-
-    return (
-      <React.Fragment>
-        <div
-          ref={(el) => {
-            this.el = el;
-            setRef(toastRef, el);
-          }}
-          {...defaultProps}
-          {...otherProps}
-        >
-          <div className="Toast__message">
-            <Icon type={typeMap[type].icon} />
-            <div className="Toast__message-content">{children}</div>
-          </div>
-          {type !== 'action-needed' && dismissible && (
-            <button
-              type="button"
-              className={'Toast__dismiss'}
-              aria-label={dismissText}
-              onClick={this.dismissToast}
-            >
-              <Icon type="close" />
-            </button>
-          )}
-        </div>
-        {scrim}
-      </React.Fragment>
-    );
-  }
-
-  dismissToast() {
-    if (!this.el) {
+  // Effect to handle `show` prop changes after mount
+  useEffect(() => {
+    if (isFirstRenderRef.current) {
+      isFirstRenderRef.current = false;
       return;
     }
-    const { onDismiss, type } = this.props;
-    const { isolator } = this.state;
 
-    this.setState(
-      {
-        animationClass: 'FadeIn--flex'
-      },
-      () => {
-        // Timeout because CSS display: none/block and opacity:
-        // 0/1 properties cannot be toggled in the same tick
-        // see: https://codepen.io/isnerms/pen/eyQaLP
-        setTimeout(() => {
-          if (type === 'action-needed') {
-            tabIndexHandler(true, this.el);
-            isolator?.deactivate();
-          }
+    if (show) {
+      setAnimationClass('FadeIn--flex');
+      const id = setTimeout(showToast);
+      return () => clearTimeout(id);
+    } else {
+      dismissToast();
+    }
+  }, [show]);
 
-          this.setState({ animationClass: 'is--hidden' }, onDismiss);
-        });
-      }
-    );
+  // Cleanup: deactivate aria isolate on unmount
+  useEffect(() => {
+    return () => {
+      isolatorRef.current?.deactivate();
+    };
+  }, []);
+
+  const scrim =
+    type === 'action-needed' && show ? (
+      <div className="Scrim--light Scrim--show Scrim--fade-in" />
+    ) : null;
+
+  const defaultProps: React.HTMLAttributes<HTMLDivElement> = {
+    tabIndex: -1,
+    className: classNames(
+      'Toast',
+      `Toast--${typeMap[type].className}`,
+      animationClass,
+      { 'Toast--non-dismissible': !dismissible },
+      className
+    )
+  };
+
+  if (!focus) {
+    defaultProps.role = 'alert';
   }
 
-  showToast() {
-    const { type, focus } = this.props;
+  return (
+    <React.Fragment>
+      <div
+        ref={(el) => {
+          elRef.current = el;
+          setRef(toastRef, el);
+        }}
+        {...defaultProps}
+        {...otherProps}
+      >
+        <div className="Toast__message">
+          <Icon type={typeMap[type].icon} />
+          <div className="Toast__message-content">{children}</div>
+        </div>
+        {type !== 'action-needed' && dismissible && (
+          <button
+            type="button"
+            className={'Toast__dismiss'}
+            aria-label={dismissText}
+            onClick={dismissToast}
+          >
+            <Icon type="close" />
+          </button>
+        )}
+      </div>
+      {scrim}
+    </React.Fragment>
+  );
+};
 
-    this.setState(
-      {
-        animationClass: 'FadeIn--flex FadeIn'
-      },
-      () => {
-        if (type === 'action-needed') {
-          const isolator = new AriaIsolate(this.el as HTMLDivElement);
-          tabIndexHandler(false, this.el);
-          this.setState({ isolator });
-          isolator.activate();
-        }
+Toast.displayName = 'Toast';
 
-        if (this.el && !!focus) {
-          // focus the toast
-          this.el.focus();
-        }
-      }
-    );
-  }
-}
+export default Toast;
